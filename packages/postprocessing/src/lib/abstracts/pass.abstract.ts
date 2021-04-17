862129eadbde5c98780c3ce3a29127a91473af29
Chau Tran <nartc7789@gmail.com>
2021-04-17 00:48:53 -0500

    feat(pass): add base inputs

--- packages/postprocessing/src/lib/abstracts/pass.abstract.ts
+++ packages/postprocessing/src/lib/abstracts/pass.abstract.ts
@@ -1,15 +1,33 @@
-import { Directive, OnDestroy, OnInit, Optional } from '@angular/core';
+import { applyProps, CanvasStore } from '@angular-three/core';
+import type { ThreeInstance } from '@angular-three/core/typings';
+import {
+  Directive,
+  Input,
+  NgZone,
+  OnDestroy,
+  OnInit,
+  Optional,
+  SkipSelf,
+} from '@angular/core';
 import { Pass } from 'three/examples/jsm/postprocessing/Pass';
 import { EffectComposerDirective } from '../effect-composer.directive';
 
 @Directive()
 export abstract class ThreePass<TPass extends Pass = Pass>
   implements OnInit, OnDestroy {
+  @Input() enabled?: boolean;
+  @Input() needsSwap?: boolean;
+  @Input() clear?: boolean;
+  @Input() renderToScreen?: boolean;
+
   constructor(
+    protected readonly ngZone: NgZone,
+    @SkipSelf() protected readonly canvasStore: CanvasStore,
     @Optional() protected readonly composer?: EffectComposerDirective
   ) {}
 
-  abstract passType: new (...args: any[]) => TPass;
+  protected abstract passType: new (...args: any[]) => TPass;
+  protected extraInputs: string[] = [];
 
   private _extraArgs: unknown[] = [];
 
@@ -20,24 +38,54 @@ export abstract class ThreePass<TPass extends Pass = Pass>
   private _pass!: TPass;
 
   ngOnInit() {
-    this._pass = new this.passType(...this._extraArgs);
-    if (this.customize) {
-      this.customize();
-    }
-    if (this.composer) {
-      this.composer.composer.addPass(this.pass);
-    }
+    this.ngZone.runOutsideAngular(() => {
+      if (this.useSceneAndCamera) {
+        const { scene, camera } = this.canvasStore.getImperativeState();
+        this._extraArgs[0] = scene;
+        this._extraArgs[1] = camera;
+      }
+      this._pass = new this.passType(...this._extraArgs);
+      this.applyExtraInputs();
+      if (this.composer) {
+        this.composer.composer.addPass(this.pass);
+      }
+    });
   }
 
   ngOnDestroy() {
-    if (this.composer) {
-      this.composer.composer.removePass(this.pass);
-    }
+    this.ngZone.runOutsideAngular(() => {
+      if (this.composer) {
+        this.composer.composer.removePass(this.pass);
+      }
+    });
   }
 
   get pass(): TPass {
     return this._pass;
   }
 
-  customize?: () => void;
+  protected get useSceneAndCamera() {
+    return false;
+  }
+
+  private applyExtraInputs(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const extraProps = [
+        'enabled',
+        'needsSwap',
+        'clear',
+        'renderToScreen',
+        ...this.extraInputs,
+      ].reduce((extraProps, extraInput) => {
+        const inputProp = ((this as unknown) as Record<string, unknown>)[
+          extraInput
+        ];
+        if (inputProp) {
+          extraProps[extraInput] = inputProp;
+        }
+        return extraProps;
+      }, {} as Record<string, unknown>);
+      applyProps((this.pass as unknown) as ThreeInstance, extraProps);
+    });
+  }
 }
