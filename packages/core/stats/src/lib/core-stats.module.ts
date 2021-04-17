48ccc90ba70388f88734c9cb0e4b7e6f945ec517
Chau Tran <nartc7789@gmail.com>
2021-04-16 20:11:50 -0500

    feat(stats): rename CoreStats to ThreeCoreStats

--- packages/core/stats/src/lib/core-stats.module.ts
+++ packages/core/stats/src/lib/core-stats.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { StatsDirective } from './stats.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [
-    StatsDirective
-  ],
-  exports: [
-    StatsDirective
-  ],
+  declarations: [StatsDirective],
+  exports: [StatsDirective],
 })
-export class CoreStatsModule {}
+export class ThreeCoreStatsModule {}
