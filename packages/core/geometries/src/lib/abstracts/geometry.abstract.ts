816748bcf2758a1c54313e70cb9f3d114b0579b9
Chau Tran <nartc7789@gmail.com>
2021-04-16 21:49:51 -0500

    fix(geometries): attribute attach/remove itself

--- packages/core/geometries/src/lib/abstracts/geometry.abstract.ts
+++ packages/core/geometries/src/lib/abstracts/geometry.abstract.ts
@@ -1,23 +1,13 @@
 import { InstancesStore } from '@angular-three/core';
-import { ThreeAttribute } from '@angular-three/core/attributes';
-import {
-  ContentChildren,
-  Directive,
-  Input,
-  OnDestroy,
-  QueryList,
-  SkipSelf,
-} from '@angular/core';
+import { Directive, Input, SkipSelf } from '@angular/core';
 import type { BufferGeometry } from 'three';
 
 @Directive()
 export abstract class ThreeBufferGeometry<
   TGeometry extends BufferGeometry = BufferGeometry,
   TGeometryConstructor extends typeof BufferGeometry = typeof BufferGeometry
-> implements OnDestroy {
+> {
   @Input() ngtId?: string;
-
-  @ContentChildren(ThreeAttribute) attributes!: QueryList<ThreeAttribute>;
 
   constructor(@SkipSelf() protected readonly instancesStore: InstancesStore) {}
 
@@ -36,29 +26,11 @@ export abstract class ThreeBufferGeometry<
         ...args: ConstructorParameters<TGeometryConstructor>
       ) => TGeometry)(...(args as ConstructorParameters<TGeometryConstructor>));
 
-      this.addAttributes();
-
       this.instancesStore.saveBufferGeometry({
         id: this.ngtId,
         bufferGeometry: this._bufferGeometry,
       });
     }
     return this._bufferGeometry;
-  }
-
-  ngOnDestroy() {
-    this.attributes.forEach(({ attach, attribute }) => {
-      if (attribute && attach) {
-        this._bufferGeometry?.deleteAttribute(attach);
-      }
-    });
-  }
-
-  private addAttributes() {
-    this.attributes.forEach(({ attach, attribute }) => {
-      if (attribute && attach) {
-        this._bufferGeometry?.setAttribute(attach, attribute);
-      }
-    });
   }
 }
