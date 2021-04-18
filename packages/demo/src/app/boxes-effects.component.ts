040e2d0e88e32bf7fa620d5d23b3bdc09da0bef8
Chau Tran <nartc7789@gmail.com>
2021-04-17 20:57:18 -0500

    docs(demo): adjust effects demo

--- packages/demo/src/app/boxes-effects.component.ts
+++ packages/demo/src/app/boxes-effects.component.ts
@@ -1,8 +1,4 @@
-import {
-  AnimationStore,
-  CanvasStore,
-  DestroyedService,
-} from '@angular-three/core';
+import { AnimationStore, CanvasStore } from '@angular-three/core';
 import { EffectComposerDirective } from '@angular-three/postprocessing';
 import {
   AfterViewInit,
@@ -10,40 +6,39 @@ import {
   Component,
   ViewChild,
 } from '@angular/core';
-import { distinctUntilKeyChanged, map, pluck, takeUntil } from 'rxjs/operators';
-import { Uniform, Vector2 } from 'three';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { distinctUntilKeyChanged, map, pluck } from 'rxjs/operators';
+import { Vector2 } from 'three';
 import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader';
 
 @Component({
   selector: 'demo-boxes-effects',
   template: `
-    <ngt-effectComposer *ngIf="aspect$ | async as aspect">
+    <ngt-effectComposer *ngIf="vm$ | async as vm">
       <ngt-renderPass></ngt-renderPass>
       <ngt-sSAOPass [kernelRadius]="0.6" [maxDistance]="0.03"></ngt-sSAOPass>
-      <ngt-unrealBloomPass [args]="[aspect, 2, 1, 0.991]"></ngt-unrealBloomPass>
+      <ngt-unrealBloomPass
+        [args]="[vm.aspect, 2, 1, 0.991]"
+      ></ngt-unrealBloomPass>
       <ngt-shaderPass
         [args]="[FXAAShader]"
         [renderToScreen]="true"
-        (ready)="onShaderPassReady($event)"
+        [assignTo]="[
+          ['material.uniforms.resolution.value', vm.resolutionValue]
+        ]"
       ></ngt-shaderPass>
     </ngt-effectComposer>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [DestroyedService],
 })
 export class BoxesEffectsComponent implements AfterViewInit {
-  readonly size$ = this.canvasStore.canvasInternal$.pipe(
+  vm$ = this.canvasStore.canvasInternal$.pipe(
     distinctUntilKeyChanged('size'),
-    pluck('size')
-  );
-
-  aspect$ = this.size$.pipe(
-    map((size) => {
-      if (this.effectComposer) {
-        this.effectComposer.composer.setSize(size.width, size.height);
-      }
-      return new Vector2(size.width, size.height);
+    pluck('size'),
+    map(({ width, height }) => {
+      return {
+        aspect: new Vector2(width, height),
+        resolutionValue: new Vector2(1 / width, 1 / height),
+      };
     })
   );
 
@@ -51,31 +46,14 @@ export class BoxesEffectsComponent implements AfterViewInit {
 
   @ViewChild(EffectComposerDirective) effectComposer?: EffectComposerDirective;
 
-  shaderPass?: ShaderPass;
-
   constructor(
     private readonly animationStore: AnimationStore,
-    private readonly canvasStore: CanvasStore,
-    private readonly destroyed: DestroyedService
+    private readonly canvasStore: CanvasStore
   ) {}
 
   ngAfterViewInit() {
-    this.size$.pipe(takeUntil(this.destroyed)).subscribe((size) => {
-      this.effectComposer?.composer.setSize(size.width, size.height);
-      if (this.shaderPass) {
-        this.shaderPass.material.uniforms.resolution = new Uniform(
-          new Vector2(1 / size.width, 1 / size.height)
-        );
-        this.shaderPass.material.needsUpdate = true;
-      }
-    });
-
     this.animationStore.registerAnimation(() => {
       this.effectComposer?.composer.render();
     }, 2);
-  }
-
-  onShaderPassReady(pass: ShaderPass) {
-    this.shaderPass = pass;
   }
 }
