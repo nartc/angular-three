cde270880f179984333dc3d1577ef2ab76dd5fa6
Chau Tran <nartc7789@gmail.com>
2021-04-18 00:26:40 -0500

    feat(core): add Scene

---
+++ packages/core/scene/README.md
@@ -0,0 +1,7 @@
+# core-scene
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test core-scene` to execute the unit tests.
---
+++ packages/core/scene/src/lib/scene.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SceneDirective } from './scene.directive';
+
+describe('SceneDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SceneDirective();
+    expect(directive).toBeTruthy();
+  });
+});
