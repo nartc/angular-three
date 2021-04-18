065cd821348f74a05e2b9a6a8a5573915815d75d
Chau Tran <nartc7789@gmail.com>
2021-04-17 20:56:32 -0500

    feat(utils): add applyDottedPathProps

--- packages/core/src/lib/utils/apply-props.util.ts
+++ packages/core/src/lib/utils/apply-props.util.ts
@@ -1,7 +1,42 @@
 import type {
   InstanceInternal,
   ThreeInstance,
+  UnknownRecord,
 } from '@angular-three/core/typings';
+
+function applyDottedPathProps(
+  instance: ThreeInstance,
+  key: string,
+  propAtKey: unknown,
+  isRoot = true
+) {
+  const [first, ...paths] = key.split('.');
+  const rootChild = ((instance as unknown) as UnknownRecord)[first];
+  if (rootChild == null) {
+    return;
+  }
+
+  if (paths.length >= 1) {
+    applyDottedPathProps(
+      (rootChild as unknown) as ThreeInstance,
+      paths.join('.'),
+      propAtKey,
+      false
+    );
+  } else {
+    ((instance as unknown) as UnknownRecord)[first] = propAtKey;
+  }
+
+  if (isRoot) {
+    checkNeedsUpdate(rootChild);
+  }
+}
+
+function checkNeedsUpdate(value: unknown) {
+  if (typeof value === 'object' && 'needsUpdate' in (value as UnknownRecord)) {
+    (value as UnknownRecord)['needsUpdate'] = true;
+  }
+}
 
 /**
  * Apply props on instances
@@ -9,10 +44,7 @@ import type {
  * @internal
  * @private
  */
-export function applyProps(
-  instance: ThreeInstance,
-  props?: Record<string, unknown>
-) {
+export function applyProps(instance: ThreeInstance, props?: UnknownRecord) {
   if (!props || (props && !Object.keys(props).length)) return;
 
   if ('__ngt' in props) {
@@ -20,46 +52,41 @@ export function applyProps(
   }
 
   if (
-    ((instance as unknown) as Record<string, unknown>)['set'] != null &&
-    typeof ((instance as unknown) as Record<string, unknown>)['set'] ===
-      'function'
+    ((instance as unknown) as UnknownRecord)['set'] != null &&
+    typeof ((instance as unknown) as UnknownRecord)['set'] === 'function'
   ) {
-    (((instance as unknown) as Record<string, unknown>)['set'] as Function)(
-      props
-    );
+    (((instance as unknown) as UnknownRecord)['set'] as Function)(props);
   }
 
   for (const [key, propAtKey] of Object.entries(props)) {
-    if (instance[key as keyof ThreeInstance] == null) {
-      ((instance as unknown) as Record<string, unknown>)[key] = propAtKey;
+    if (key.split('.').length > 1) {
+      applyDottedPathProps(instance, key, propAtKey);
     } else {
-      if (
-        (instance[key as keyof ThreeInstance] as Record<string, unknown>)[
-          'set'
-        ] != null &&
-        typeof (instance[key as keyof ThreeInstance] as Record<
-          string,
-          unknown
-        >)['set'] === 'function'
-      ) {
-        if (Array.isArray(propAtKey)) {
-          ((instance[key as keyof ThreeInstance] as Record<string, unknown>)[
-            'set'
-          ] as Function)(...propAtKey);
-        } else {
-          ((instance[key as keyof ThreeInstance] as Record<string, unknown>)[
-            'set'
-          ] as Function)(propAtKey);
-        }
+      if (instance[key as keyof ThreeInstance] == null) {
+        ((instance as unknown) as UnknownRecord)[key] = propAtKey;
       } else {
-        ((instance as unknown) as Record<string, unknown>)[key] = propAtKey;
+        if (
+          (instance[key as keyof ThreeInstance] as UnknownRecord)['set'] !=
+            null &&
+          typeof (instance[key as keyof ThreeInstance] as Record<
+            string,
+            unknown
+          >)['set'] === 'function'
+        ) {
+          if (Array.isArray(propAtKey)) {
+            ((instance[key as keyof ThreeInstance] as UnknownRecord)[
+              'set'
+            ] as Function)(...propAtKey);
+          } else {
+            ((instance[key as keyof ThreeInstance] as UnknownRecord)[
+              'set'
+            ] as Function)(propAtKey);
+          }
+        } else {
+          ((instance as unknown) as UnknownRecord)[key] = propAtKey;
+        }
       }
-    }
-    if (
-      typeof propAtKey === 'object' &&
-      'needsUpdate' in (propAtKey as Record<string, unknown>)
-    ) {
-      (propAtKey as Record<string, unknown>)['needsUpdate'] = true;
+      checkNeedsUpdate(propAtKey);
     }
   }
 }
