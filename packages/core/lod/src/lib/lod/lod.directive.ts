a30e2d3bb6a30b04636db94a1a251c036528de42
Chau Tran <nartc7789@gmail.com>
2021-04-22 13:32:02 -0500

    fix(core): rework LOD

--- packages/core/lod/src/lib/lod/lod.directive.ts
+++ packages/core/lod/src/lib/lod/lod.directive.ts
@@ -1,6 +1,6 @@
 import { ThreeObject3d } from '@angular-three/core';
-import type { OnInit, QueryList } from '@angular/core';
-import { AfterContentInit, ContentChildren, Directive } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { Directive } from '@angular/core';
 import { LOD } from 'three';
 
 @Directive({
@@ -8,27 +8,11 @@ import { LOD } from 'three';
   exportAs: 'ngtLod',
   providers: [{ provide: ThreeObject3d, useExisting: LodDirective }],
 })
-export class LodDirective
-  extends ThreeObject3d<LOD>
-  implements OnInit, AfterContentInit {
+export class LodDirective extends ThreeObject3d<LOD> implements OnInit {
   private _lod!: LOD;
-
-  @ContentChildren(ThreeObject3d) objectLevels?: QueryList<ThreeObject3d>;
 
   ngOnInit() {
     this.init();
-  }
-
-  ngAfterContentInit() {
-    this.ngZone.runOutsideAngular(() => {
-      if (this.objectLevels) {
-        this.objectLevels.forEach((objectLevel) => {
-          if (objectLevel.object3d) {
-            this._lod.addLevel(objectLevel.object3d, objectLevel.lodDistance);
-          }
-        });
-      }
-    });
   }
 
   protected initObject() {
--- packages/core/src/lib/abstracts/object-3d.abstract.ts
+++ packages/core/src/lib/abstracts/object-3d.abstract.ts
@@ -48,8 +48,6 @@ export abstract class ThreeObject3d<TObject extends Object3D = Object3D>
   @Input() visible = true;
   @Input() matrixAutoUpdate = true;
 
-  @Input() lodDistance?: number;
-
   @Input() appendMode: 'immediate' | 'root' = 'immediate';
   @Input() appendTo?: Object3D;
 
@@ -75,13 +73,14 @@ export abstract class ThreeObject3d<TObject extends Object3D = Object3D>
   }
 
   @Output() ready = this.object3d$.pipe(filter(Boolean)) as Observable<TObject>;
+  @Output() zonelessReady = new EventEmitter<TObject>();
 
   constructor(
     protected readonly canvasStore: CanvasStore,
-    protected readonly animationStore: AnimationStore,
     protected readonly instancesStore: InstancesStore,
     protected readonly eventsStore: EventsStore,
-    protected readonly ngZone: NgZone,
+    readonly animationStore: AnimationStore,
+    readonly ngZone: NgZone,
     @Optional()
     @SkipSelf()
     protected readonly parentObjectDirective?: ThreeObject3d
@@ -153,6 +152,7 @@ export abstract class ThreeObject3d<TObject extends Object3D = Object3D>
       this.$object3d.next(this.object3d);
     });
 
+    this.zonelessReady.emit(this.object3d);
     this.participate(this.object3d);
   }
 
