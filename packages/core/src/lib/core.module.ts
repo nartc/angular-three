a970375f24b4d1db595cd99e816d0e4b65b03ef0
Chau Tran <nartc7789@gmail.com>
2021-05-31 23:36:25 -0500

    refactor(core): remove MathConstantPipe and MathPipe from CoreModule

--- packages/core/src/lib/core.module.ts
+++ packages/core/src/lib/core.module.ts
@@ -2,22 +2,9 @@ import { NgModule } from '@angular/core';
 import { CanvasComponent } from './canvas.component';
 import { Object3dControllerDirective } from './controllers';
 import { RepeatDirective } from './directives';
-import { MathConstantPipe, MathPipe } from './pipes';
 
 @NgModule({
-  declarations: [
-    CanvasComponent,
-    Object3dControllerDirective,
-    MathPipe,
-    MathConstantPipe,
-    RepeatDirective,
-  ],
-  exports: [
-    CanvasComponent,
-    Object3dControllerDirective,
-    MathPipe,
-    MathConstantPipe,
-    RepeatDirective,
-  ],
+  declarations: [CanvasComponent, Object3dControllerDirective, RepeatDirective],
+  exports: [CanvasComponent, Object3dControllerDirective, RepeatDirective],
 })
 export class ThreeCoreModule {}
