25b2ea978922d62c5719fdc66d7dd924480fec3e
Chau Tran <nartc7789@gmail.com>
2021-05-31 21:15:15 -0500

    feat(core): add RepeatDirective

--- packages/core/src/lib/core.module.ts
+++ packages/core/src/lib/core.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CanvasComponent } from './canvas.component';
 import { Object3dControllerDirective } from './controllers';
+import { RepeatDirective } from './directives';
+import { MathConstantPipe, MathPipe } from './pipes';
 
 @NgModule({
-  declarations: [CanvasComponent, Object3dControllerDirective],
-  exports: [CanvasComponent, Object3dControllerDirective],
+  declarations: [
+    CanvasComponent,
+    Object3dControllerDirective,
+    MathPipe,
+    MathConstantPipe,
+    RepeatDirective,
+  ],
+  exports: [
+    CanvasComponent,
+    Object3dControllerDirective,
+    MathPipe,
+    MathConstantPipe,
+    RepeatDirective,
+  ],
 })
 export class ThreeCoreModule {}
