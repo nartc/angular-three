dfd0eb32502a86c430cd02c01c5b04099be604d9
Chau Tran <nartc7789@gmail.com>
2021-06-12 22:00:18 -0500

    fix(core): limit to 60fps

--- packages/core/src/lib/services/loop.service.ts
+++ packages/core/src/lib/services/loop.service.ts
@@ -5,6 +5,8 @@ import { AnimationStore, CanvasStore } from '../stores';
 @Injectable()
 export class LoopService implements OnDestroy {
   private readonly loopClock = new Clock();
+  private readonly interval = 1 / 60; // 60fps;
+  private internalDelta = 0;
 
   constructor(
     private readonly canvasStore: CanvasStore,
@@ -28,43 +30,49 @@ export class LoopService implements OnDestroy {
   }
 
   private tick(delta: number) {
-    const {
-      renderer,
-      scene,
-      camera,
-      internal: { size, viewport },
-      clock,
-      mouse,
-    } = this.canvasStore.getImperativeState();
-    const { animationCallbacks, hasPriority } =
-      this.animationStore.getImperativeState();
+    this.internalDelta += delta;
 
-    if (renderer && scene && camera) {
-      if (hasPriority) {
-        animationCallbacks.sort(
-          ({ priority: a = 0 }, { priority: b = 0 }) => a - b
-        );
-      } else {
-        renderer.render(scene, camera);
-      }
-
-      const renderState = {
-        clock,
-        camera,
-        scene,
+    if (this.internalDelta > this.interval) {
+      const {
         renderer,
+        scene,
+        camera,
+        internal: { size, viewport },
+        clock,
         mouse,
-        size,
-        viewport,
-        delta,
-      };
-      for (const animationCallback of animationCallbacks) {
-        if (animationCallback.obj) {
-          animationCallback.callback(animationCallback.obj, renderState);
+      } = this.canvasStore.getImperativeState();
+      const { animationCallbacks, hasPriority } =
+        this.animationStore.getImperativeState();
+
+      if (renderer && scene && camera) {
+        if (hasPriority) {
+          animationCallbacks.sort(
+            ({ priority: a = 0 }, { priority: b = 0 }) => a - b
+          );
         } else {
-          animationCallback.callback(renderState);
+          renderer.render(scene, camera);
+        }
+
+        const renderState = {
+          clock,
+          camera,
+          scene,
+          renderer,
+          mouse,
+          size,
+          viewport,
+          delta,
+        };
+        for (const animationCallback of animationCallbacks) {
+          if (animationCallback.obj) {
+            animationCallback.callback(animationCallback.obj, renderState);
+          } else {
+            animationCallback.callback(renderState);
+          }
         }
       }
+
+      this.internalDelta = this.internalDelta % this.interval;
     }
   }
 
