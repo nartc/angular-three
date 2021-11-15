54adeaefea7eb0510520666123856d6db9a6f661
Chau Tran <nartc7789@gmail.com>
2021-11-15 13:06:06 -0600

    feat(postprocessing): add abstract pass

---
+++ packages/postprocessing/src/lib/effect-composer.directive.ts
@@ -0,0 +1,81 @@
+import { CanvasStore } from '@angular-three/core';
+import {
+  Directive,
+  EventEmitter,
+  Input,
+  NgZone,
+  OnInit,
+  Output,
+  SkipSelf,
+} from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import {
+  distinctUntilKeyChanged,
+  EMPTY,
+  map,
+  switchMap,
+  tap,
+  withLatestFrom,
+} from 'rxjs';
+import * as THREE from 'three';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+
+@Directive({
+  selector: 'ngt-effect-composer',
+  exportAs: 'ngtEffectComposer',
+})
+export class NgtEffectComposer extends ComponentStore<{}> implements OnInit {
+  @Input() renderTarget?: THREE.WebGLRenderTarget;
+  @Input() watchSizeChanged = true;
+  @Output() ready = new EventEmitter<EffectComposer>();
+
+  private _composer!: EffectComposer;
+
+  get composer(): EffectComposer {
+    return this._composer;
+  }
+
+  constructor(
+    private ngZone: NgZone,
+    @SkipSelf() private canvasStore: CanvasStore
+  ) {
+    super({});
+  }
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.initComposer(
+        this.canvasStore.selectors.internal$.pipe(
+          distinctUntilKeyChanged('active'),
+          map((internal) => internal.active)
+        )
+      );
+    });
+  }
+
+  private readonly initComposer = this.effect<boolean>((active$) =>
+    active$.pipe(
+      withLatestFrom(this.canvasStore.selectors.renderer$),
+      switchMap(([active, renderer]) => {
+        if (active && renderer) {
+          this._composer = new EffectComposer(renderer, this.renderTarget);
+          this.ready.emit(this.composer);
+
+          if (this.watchSizeChanged) {
+            return this.canvasStore.selectors.internal$.pipe(
+              distinctUntilKeyChanged('size'),
+              map((internal) => internal.size),
+              tap(({ width, height }) => {
+                this.ngZone.runOutsideAngular(() => {
+                  this._composer.setSize(width, height);
+                });
+              })
+            );
+          }
+        }
+
+        return EMPTY;
+      })
+    )
+  );
+}
