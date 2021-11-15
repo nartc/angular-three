0e3c5bad661e6a2b48a6694c279306b2d4794a2f
Chau Tran <nartc7789@gmail.com>
2021-11-14 22:13:36 -0600

    fix: remove line examples

--- tools/generators/three/entities/lines.ts
+++ tools/generators/three/entities/lines.ts
@@ -3,5 +3,4 @@ import type { EntityCollection } from '../models/entity-collection.model';
 
 export const lines: EntityCollection = {
   core: [THREE.Line, THREE.LineLoop, THREE.LineSegments].map((m) => m.name),
-  examples: ['Line2', 'LineSegments2', 'Wireframe'],
 };
