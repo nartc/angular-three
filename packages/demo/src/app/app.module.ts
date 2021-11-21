1ffccb34c061777ab55464c6fdde35507191b334
Chau Tran <nartc7789@gmail.com>
2021-11-20 22:47:21 -0600

    docs(demo): add kinematic cube demo

--- packages/demo/src/app/app.module.ts
+++ packages/demo/src/app/app.module.ts
@@ -1,11 +1,89 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgtCannonModule } from '@angular-three/cannon';
+import { NgtPhysicBoxModule } from '@angular-three/cannon/box';
+import { NgtPhysicPlaneModule } from '@angular-three/cannon/plane';
+import { NgtPhysicSphereModule } from '@angular-three/cannon/sphere';
+import { NgtOrbitControlsModule } from '@angular-three/controls/orbit-controls';
+import {
+  NgtCoreModule,
+  NgtMathPipeModule,
+  NgtVectorPipeModule,
+} from '@angular-three/core';
+import { NgtInstancedBufferAttributeModule } from '@angular-three/core/attributes';
+import {
+  NgtBoxGeometryModule,
+  NgtCylinderGeometryModule,
+  NgtPlaneGeometryModule,
+  NgtSphereGeometryModule,
+} from '@angular-three/core/geometries';
+import {
+  NgtCameraHelperModule,
+  NgtSpotLightHelperModule,
+} from '@angular-three/core/helpers';
+import {
+  NgtAmbientLightModule,
+  NgtDirectionalLightModule,
+  NgtHemisphereLightModule,
+  NgtPointLightModule,
+  NgtSpotLightModule,
+} from '@angular-three/core/lights';
+import {
+  NgtMeshBasicMaterialModule,
+  NgtMeshLambertMaterialModule,
+  NgtMeshPhongMaterialModule,
+  NgtShadowMaterialModule,
+} from '@angular-three/core/materials';
+import {
+  NgtInstancedMeshModule,
+  NgtMeshModule,
+} from '@angular-three/core/meshes';
+import { NgtStatsModule } from '@angular-three/core/stats';
 
-import { AppComponent } from './app.component';
+import {
+  AppComponent,
+  BoxComponent,
+  InstancedSpheresComponent,
+  PlaneComponent,
+} from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    PlaneComponent,
+    BoxComponent,
+    InstancedSpheresComponent,
+  ],
+  imports: [
+    BrowserModule,
+    NgtCoreModule,
+    NgtMeshModule,
+    NgtAmbientLightModule,
+    NgtDirectionalLightModule,
+    NgtBoxGeometryModule,
+    NgtMeshPhongMaterialModule,
+    NgtCannonModule,
+    NgtPhysicBoxModule,
+    NgtPlaneGeometryModule,
+    NgtShadowMaterialModule,
+    NgtPhysicPlaneModule,
+    NgtMeshBasicMaterialModule,
+    NgtMathPipeModule,
+    NgtMeshLambertMaterialModule,
+    NgtInstancedMeshModule,
+    NgtPhysicSphereModule,
+    NgtSphereGeometryModule,
+    NgtInstancedBufferAttributeModule,
+    NgtHemisphereLightModule,
+    NgtSpotLightModule,
+    NgtPointLightModule,
+    NgtSpotLightHelperModule,
+    NgtOrbitControlsModule,
+    NgtCameraHelperModule,
+    NgtCylinderGeometryModule,
+    NgtStatsModule,
+    NgtVectorPipeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
