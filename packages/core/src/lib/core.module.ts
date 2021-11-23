b387f2e5a1674d2968acbd7b68b2f1ee93e22791
Chau Tran <nartc7789@gmail.com>
2021-11-23 13:59:42 -0600

    feat(core): expose AudioController

--- packages/core/src/lib/core.module.ts
+++ packages/core/src/lib/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NgtCanvasComponent } from './canvas.component';
+import { NgtAudioController } from './three/audio.controller';
 import { NgtObject3dController } from './three/object-3d.controller';
 
 @NgModule({
-  declarations: [NgtCanvasComponent, NgtObject3dController],
-  exports: [NgtCanvasComponent, NgtObject3dController],
+  declarations: [NgtCanvasComponent, NgtObject3dController, NgtAudioController],
+  exports: [NgtCanvasComponent, NgtObject3dController, NgtAudioController],
 })
 export class NgtCoreModule {}
