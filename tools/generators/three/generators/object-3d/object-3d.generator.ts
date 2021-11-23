1e20bc966ce6302198a469078b07d5ca54b0b411
Chau Tran <nartc7789@gmail.com>
2021-11-23 14:01:40 -0600

    fix: add additionalSobaSelectors to object-3d generator

--- tools/generators/three/generators/object-3d/object-3d.generator.ts
+++ tools/generators/three/generators/object-3d/object-3d.generator.ts
@@ -1,6 +1,15 @@
 import { generateFiles, getWorkspaceLayout, Tree } from '@nrwl/devkit';
 import { join } from 'path';
 
+const additionalSobaSelectors = [
+  'ngt-soba-image',
+  'ngt-soba-text',
+  'ngt-soba-line',
+  'ngt-soba-quadratic-bezier-line',
+  'ngt-soba-cubic-bezier-line',
+  'ngt-soba-positional-audio',
+] as const;
+
 async function object3dGenerator(tree: Tree, derivedObject3Ds: string[]) {
   const { libsDir } = getWorkspaceLayout(tree);
   generateFiles(
@@ -8,10 +17,12 @@ async function object3dGenerator(tree: Tree, derivedObject3Ds: string[]) {
     join(__dirname, 'files'),
     join(libsDir, 'core', 'src', 'lib', 'three'),
     {
-      selectors: derivedObject3Ds.map((selector, index) => ({
-        selector,
-        isLast: index === derivedObject3Ds.length - 1,
-      })),
+      selectors: [...derivedObject3Ds, ...additionalSobaSelectors].map(
+        (selector, index) => ({
+          selector,
+          isLast: index === derivedObject3Ds.length - 1,
+        })
+      ),
       tmpl: '',
     }
   );
