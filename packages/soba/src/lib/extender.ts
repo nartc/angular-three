2dae4924ea320b20f5123ac7b567a05ed90840fa
Chau Tran <nartc7789@gmail.com>
2021-11-23 23:53:26 -0600

    fix(soba): remove ng-content and use [material] on mesh instead

--- packages/soba/src/lib/extender.ts
+++ packages/soba/src/lib/extender.ts
@@ -1,8 +1,10 @@
-import { NgtAnimationReady } from '@angular-three/core';
-import { Directive, EventEmitter, Output } from '@angular/core';
+import { NgtAnimationReady, NgtMaterial } from '@angular-three/core';
+import { ContentChild, Directive, EventEmitter, Output } from '@angular/core';
 
 @Directive()
 export abstract class NgtSobaExtender<TEntity> {
   @Output() ready = new EventEmitter<TEntity>();
   @Output() animateReady = new EventEmitter<NgtAnimationReady<TEntity>>();
+
+  @ContentChild(NgtMaterial) material?: NgtMaterial;
 }
