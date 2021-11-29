cf41a0142018c8b9ba3b4c572fbec182239bc033
Chau Tran <nartc7789@gmail.com>
2021-11-29 15:37:27 -0600

    feat(core): add attributes generator

--- tools/generators/three/index.ts
+++ tools/generators/three/index.ts
@@ -1,7 +1,12 @@
 import { Tree } from '@nrwl/devkit';
+import attributesGenerator from './attributes/attributes';
 import geometriesGenerator from './geometries/geometries';
 import materialsGenerator from './materials/materials';
 
 export default async function (tree: Tree) {
-  await Promise.all([materialsGenerator(tree), geometriesGenerator(tree)]);
+  await Promise.all([
+    materialsGenerator(tree),
+    geometriesGenerator(tree),
+    attributesGenerator(tree),
+  ]);
 }
