8aa055cf9fa5e9f89b077f8b84c5c71965c64fc9
Chau Tran <nartc7789@gmail.com>
2021-11-29 16:26:37 -0600

    feat(core): add primitive

---
+++ packages/core/primitive/src/lib/primitive.directive.ts
@@ -0,0 +1,40 @@
+import {
+  NGT_OBJECT_CONTROLLER_PROVIDER,
+  NGT_OBJECT_WATCHED_CONTROLLER,
+  NgtObject3dController,
+} from '@angular-three/core';
+import { Directive, Inject, Input, NgModule } from '@angular/core';
+import * as THREE from 'three';
+
+@Directive({
+  selector: 'ngt-primitive[object]',
+  exportAs: 'ngtPrimitive',
+  providers: [NGT_OBJECT_CONTROLLER_PROVIDER],
+})
+export class NgtPrimitive {
+  #object!: THREE.Object3D;
+
+  @Input() set object(value: THREE.Object3D) {
+    this.#object = value;
+    this.objectController.initFn = () => value;
+  }
+
+  get object() {
+    return this.#object;
+  }
+
+  constructor(
+    @Inject(NGT_OBJECT_WATCHED_CONTROLLER)
+    private objectController: NgtObject3dController
+  ) {}
+
+  ngOnChanges() {
+    this.objectController.init();
+  }
+}
+
+@NgModule({
+  declarations: [NgtPrimitive],
+  exports: [NgtPrimitive],
+})
+export class NgtPrimitiveModule {}
