cd9f8b78c67834a89064a46e635e656c1c6086b8
Chau Tran <nartc7789@gmail.com>
2021-12-01 20:13:27 -0600

    feat(core): add ColorPIpe

---
+++ packages/core/src/lib/color/color.pipe.ts
@@ -0,0 +1,22 @@
+import { NgModule, Pipe, PipeTransform } from '@angular/core';
+import * as THREE from 'three';
+import { NgtColor } from '../models';
+import { makeColor } from '../utils/make';
+
+@Pipe({
+  name: 'color',
+})
+export class NgtColorPipe implements PipeTransform {
+  /**
+   * ConstructorParameters<typeof THREE.Color> has a limitation on THREE.Color constructor overloads
+   */
+  transform(args: NgtColor): THREE.Color {
+    return makeColor(args) as THREE.Color;
+  }
+}
+
+@NgModule({
+  declarations: [NgtColorPipe],
+  exports: [NgtColorPipe],
+})
+export class NgtColorPipeModule {}
