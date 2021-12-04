73ed59c132ee8afcef5ca1bb4fdb1526f65aaf79
Chau Tran <nartc7789@gmail.com>
2021-12-04 12:19:55 -0600

    docs(demo): clean up

--- packages/demo/src/app/app.component.ts
+++ packages/demo/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngt-root',
   template: `
-    <!--    <ngt-simple-cube></ngt-simple-cube>-->
+    <ngt-simple-cube></ngt-simple-cube>
     <!--    <ngt-kinematic-cube></ngt-kinematic-cube>-->
     <!--    <ngt-compound-body></ngt-compound-body>-->
-    <ngt-keen-bloom></ngt-keen-bloom>
+    <!--    <ngt-keen-bloom></ngt-keen-bloom>-->
   `,
 })
 export class AppComponent {}
--- packages/demo/src/app/app.module.ts
+++ packages/demo/src/app/app.module.ts
@@ -1,17 +1,3 @@
-import { NgtCoreModule } from '@angular-three/core';
-import { NgtBoxGeometryModule } from '@angular-three/core/geometries';
-import {
-  NgtAmbientLightModule,
-  NgtSpotLightModule,
-} from '@angular-three/core/lights';
-import {
-  NgtMeshBasicMaterialModule,
-  NgtMeshPhongMaterialModule,
-  NgtMeshStandardMaterialModule,
-} from '@angular-three/core/materials';
-import { NgtMeshModule } from '@angular-three/core/meshes';
-import { NgtStatsModule } from '@angular-three/core/stats';
-import { NgtSobaOrbitControlsModule } from '@angular-three/soba/controls';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -25,16 +11,6 @@ import { SimpleCubeComponentModule } from './simple-cube.component';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    NgtCoreModule,
-    NgtMeshModule,
-    NgtMeshBasicMaterialModule,
-    NgtBoxGeometryModule,
-    NgtStatsModule,
-    NgtMeshPhongMaterialModule,
-    NgtAmbientLightModule,
-    NgtSpotLightModule,
-    NgtMeshStandardMaterialModule,
-    NgtSobaOrbitControlsModule,
     SimpleCubeComponentModule,
     KinematicCubeComponentModule,
     CompoundBodyComponentModule,
