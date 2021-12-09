bce4199cefa470672059211ea91b4bca8049834a
Chau Tran <nartc7789@gmail.com>
2021-12-08 20:57:52 -0600

    docs(demo): adjust level of details demo

--- packages/demo/src/app/app.component.ts
+++ packages/demo/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngt-root',
   template: `
-    <ngt-simple-cube></ngt-simple-cube>
+    <!--    <ngt-simple-cube></ngt-simple-cube>-->
     <!--    <ngt-kinematic-cube></ngt-kinematic-cube>-->
     <!--    <ngt-compound-body></ngt-compound-body>-->
     <!--    <ngt-keen-bloom></ngt-keen-bloom>-->
     <!--    <ngt-instances></ngt-instances>-->
+    <ngt-level-of-details></ngt-level-of-details>
   `,
 })
 export class AppComponent {}
