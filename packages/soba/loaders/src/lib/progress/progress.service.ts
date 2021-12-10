ed0dedbec93a6dfb288d85e897c8456e8aff824c
Chau Tran <nartc7789@gmail.com>
2021-12-10 16:52:36 -0600

    feat(soba): add progress service

---
+++ packages/soba/loaders/src/lib/progress/progress.service.ts
@@ -0,0 +1,55 @@
+import { EnhancedRxState } from '@angular-three/core';
+import { Injectable } from '@angular/core';
+import * as THREE from 'three';
+
+interface NgtProgressResult {
+  errors: string[];
+  active: boolean;
+  progress: number;
+  item: string;
+  loaded: number;
+  total: number;
+}
+
+@Injectable()
+export class NgtSobaProgress extends EnhancedRxState<NgtProgressResult> {
+  constructor() {
+    super();
+    let saveLastTotalLoaded = 0;
+    this.set({ active: false, errors: [] });
+    THREE.DefaultLoadingManager.onStart = (item, loaded, total) => {
+      this.set({
+        active: true,
+        item,
+        loaded,
+        total,
+        progress:
+          ((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) *
+          100,
+      });
+    };
+
+    THREE.DefaultLoadingManager.onLoad = () => {
+      this.set({ active: false });
+    };
+
+    THREE.DefaultLoadingManager.onError = (url: string) => {
+      this.set((state) => ({ errors: [...state.errors, url] }));
+    };
+
+    THREE.DefaultLoadingManager.onProgress = (item, loaded, total) => {
+      if (loaded === total) {
+        saveLastTotalLoaded = total;
+      }
+      this.set({
+        active: true,
+        item,
+        loaded,
+        total,
+        progress:
+          ((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) *
+            100 || 100,
+      });
+    };
+  }
+}
