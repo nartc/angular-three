4a22eecd3a29a6b920efc1e27775d678d77ca337
Chau Tran <nartc7789@gmail.com>
2021-12-14 20:08:35 -0600

    docs(demo/storybook): fix compound body demo

--- packages/demo/src/app/app.component.ts
+++ packages/demo/src/app/app.component.ts
@@ -5,11 +5,11 @@ import { Component } from '@angular/core';
   template: `
     <!--    <ngt-simple-cube></ngt-simple-cube>-->
     <!--    <ngt-kinematic-cube></ngt-kinematic-cube>-->
-    <!--    <ngt-compound-body></ngt-compound-body>-->
+    <ngt-compound-body></ngt-compound-body>
     <!--    <ngt-keen-bloom></ngt-keen-bloom>-->
     <!--    <ngt-instances></ngt-instances>-->
     <!--    <ngt-level-of-details></ngt-level-of-details>-->
-    <ngt-robot-expressive></ngt-robot-expressive>
+    <!--    <ngt-robot-expressive></ngt-robot-expressive>-->
   `,
 })
 export class AppComponent {}
