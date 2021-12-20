9708addf50c9faa9eadfb78f9911f429e64de243
Chau Tran <nartc7789@gmail.com>
2021-12-19 21:25:58 -0600

    docs(demo/storybook): clean up orbit controls swtich target demo

--- packages/demo/src/app/orbit-controls-target-switcher/orbit-controls-target-switcher.component.ts
+++ packages/demo/src/app/orbit-controls-target-switcher/orbit-controls-target-switcher.component.ts
@@ -1,11 +1,7 @@
 import { makeVector3, NgtCoreModule } from '@angular-three/core';
 import { NgtBoxGeometryModule } from '@angular-three/core/geometries';
 import { NgtGroupModule } from '@angular-three/core/group';
-import {
-  NgtGridHelperModule,
-  NgtPolarGridHelperModule,
-  NgtSpotLightHelperModule,
-} from '@angular-three/core/helpers';
+import { NgtGridHelperModule } from '@angular-three/core/helpers';
 import {
   NgtAmbientLightModule,
   NgtSpotLightModule,
@@ -14,28 +10,24 @@ import {
   NgtMeshPhongMaterialModule,
   NgtMeshStandardMaterialModule,
 } from '@angular-three/core/materials';
-import { NgtMesh, NgtMeshModule } from '@angular-three/core/meshes';
+import { NgtMeshModule } from '@angular-three/core/meshes';
 import { NgtStatsModule } from '@angular-three/core/stats';
-import {
-  NgtSobaOrbitControls,
-  NgtSobaOrbitControlsModule,
-} from '@angular-three/soba/controls';
-import { CommonModule } from '@angular/common';
+import { NgtSobaTextModule } from '@angular-three/soba/abstractions';
+import { NgtSobaOrbitControlsModule } from '@angular-three/soba/controls';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  Inject,
   NgModule,
   NgZone,
-  OnInit,
-  QueryList,
-  ViewChild,
-  ViewChildren,
 } from '@angular/core';
 import { gsap } from 'gsap';
 
 // @ts-ignore
 import niceColors from 'nice-color-palettes';
-import { Vector3 } from 'three';
+import { Mesh, Vector3 } from 'three';
+import { OrbitControls } from 'three-stdlib';
 import { SimpleCubeComponentModule } from '../simple-cube.component';
 
 export interface CubeContent {
@@ -49,48 +41,47 @@ export interface CubeContent {
 @Component({
   selector: 'ngt-orbit-controls-target-switcher',
   template: `
-    <ngt-canvas [scene]="{ background: 'white' | color }">
+    <ngt-canvas>
       <ngt-stats></ngt-stats>
-      <ngt-ambient-light></ngt-ambient-light>
 
+      <ngt-ambient-light></ngt-ambient-light>
       <ngt-spot-light [position]="[1, 10, 1]"></ngt-spot-light>
 
       <ngt-group [position]="[0, 2, 0]">
-        <ng-template
-          ngFor
-          [ngForOf]="cubeContents"
-          let-cubeContent
-          let-i="index"
+        <ngt-group
+          *ngFor="let cubeContent of cubeContents; let i = index"
+          [rotation]="[0, ((2 | mathConst: 'PI') / cubeContents.length) * i, 0]"
         >
-          <ngt-group
-            [rotation]="[
-              0,
-              ((2 | mathConst: 'PI') / cubeContents.length) * i,
-              0
-            ]"
+          <ngt-soba-text
+            [position]="[6, 1, 0]"
+            [fontSize]="1"
+            [color]="cubeContent.color"
           >
-            <ngt-mesh
-              #cube
-              [position]="[6, 0, 0]"
-              (click)="selectedCube = cubeContent"
-              [userData]="cubeContent"
-            >
-              <ngt-box-geometry [args]="[1, 1, 1]"></ngt-box-geometry>
-              <ngt-mesh-standard-material
-                [parameters]="{ color: cubeContent.color }"
-              ></ngt-mesh-standard-material>
-            </ngt-mesh>
-          </ngt-group>
-        </ng-template>
+            {{ cubeContent.label }}
+          </ngt-soba-text>
+          <ngt-mesh
+            #cube="ngtMesh"
+            (ready)="cubes.push(cube.mesh)"
+            (click)="onCubeSelected(cubeContent, i)"
+            (pointerover)="onHover()"
+            (pointerout)="offHover()"
+            [position]="[6, 0, 0]"
+            [userData]="cubeContent"
+          >
+            <ngt-box-geometry></ngt-box-geometry>
+            <ngt-mesh-standard-material
+              [parameters]="{ color: cubeContent.color }"
+            ></ngt-mesh-standard-material>
+          </ngt-mesh>
+        </ngt-group>
       </ngt-group>
 
       <ngt-grid-helper
         [args]="[200, 40, '#ED6307', '#8E8E8E']"
-        [receiveShadow]="true"
       ></ngt-grid-helper>
 
       <ngt-soba-orbit-controls
-        #orbitControls
+        (ready)="orbitControls = $event"
         [target]="[0, 2, 0]"
       ></ngt-soba-orbit-controls>
     </ngt-canvas>
@@ -98,26 +89,22 @@ export interface CubeContent {
     <nav>
       <div>
         <span
-          (click)="selectedCube = null"
-          [innerHTML]="'start'"
+          (click)="onCubeSelected(null, -1)"
           [class.active]="selectedCube === null"
-        ></span>
-        <ng-template
-          ngFor
-          [ngForOf]="cubeContents"
-          let-cubeContent
-          let-i="index"
         >
-          <span
-            (click)="selectedCube = cubeContent"
-            [innerHTML]="cubeContent.label"
-            [class.active]="cubeContent.id === selectedCube?.id"
-          ></span>
-        </ng-template>
+          start
+        </span>
+        <span
+          *ngFor="let cubeContent of cubeContents; index as i"
+          (click)="onCubeSelected(cubeContent, i)"
+          [class.active]="cubeContent.id === selectedCube?.id"
+        >
+          {{ cubeContent.label }}
+        </span>
       </div>
-      <small
-        >select a cube or a menu item to update Orbit Controls Target</small
-      >
+      <small>
+        select a cube or a menu item to update Orbit Controls Target
+      </small>
     </nav>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -162,75 +149,73 @@ export interface CubeContent {
     `,
   ],
 })
-export class OrbitControlsTargetSwitcherComponent implements OnInit {
+export class OrbitControlsTargetSwitcherComponent {
   cubeContents: CubeContent[] = [
     {
       id: 0,
       label: 'TRGT1',
-      color: this.getRandomColor()[0],
+      color: this.#getRandomColor()[0],
     },
     {
       id: 1,
       label: 'TRGT2',
-      color: this.getRandomColor()[1],
+      color: this.#getRandomColor()[1],
     },
     {
       id: 2,
       label: 'TRGT3',
-      color: this.getRandomColor()[2],
+      color: this.#getRandomColor()[2],
     },
     {
       id: 3,
       label: 'TRGT4',
-      color: this.getRandomColor()[3],
+      color: this.#getRandomColor()[3],
     },
     {
       id: 4,
       label: 'TRGT5',
-      color: this.getRandomColor()[4],
+      color: this.#getRandomColor()[4],
     },
   ];
 
-  @ViewChild('orbitControls', { read: NgtSobaOrbitControls })
-  orbitControls!: NgtSobaOrbitControls;
-
-  @ViewChildren('cube', { read: NgtMesh }) cubeMeshes!: QueryList<NgtMesh>;
+  cubes: Mesh[] = [];
+  orbitControls!: OrbitControls;
 
   #selectedCube: CubeContent | null = null;
-
-  set selectedCube(value: CubeContent | null) {
-    this.#selectedCube = value;
-    if (this.cubeMeshes) {
-      const cubeContentIndex = this.cubeContents.findIndex((cubeContent) => {
-        return value?.id === cubeContent.id;
-      });
-      const orbitControlsTarget = makeVector3(
-        this.orbitControls.controls.target
-      );
-      if (orbitControlsTarget) {
-        if (cubeContentIndex > -1) {
-          const target: Vector3 = new Vector3();
-          const cubeMesh = this.cubeMeshes.get(cubeContentIndex);
-          if (cubeMesh) {
-            cubeMesh.mesh.getWorldPosition(target);
-            this.tweenOrbitControls(orbitControlsTarget, target);
-          }
-        } else {
-          this.tweenOrbitControls(orbitControlsTarget, new Vector3(0, 2, 0));
-        }
-      }
-    }
-  }
 
   get selectedCube(): CubeContent | null {
     return this.#selectedCube;
   }
 
-  constructor(private ngZone: NgZone) {}
+  constructor(
+    private ngZone: NgZone,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
-  ngOnInit(): void {}
+  onHover() {
+    this.document.body.style.cursor = 'pointer';
+  }
 
-  tweenOrbitControls(source: Vector3, target: Vector3) {
+  offHover() {
+    this.document.body.style.cursor = 'auto';
+  }
+
+  onCubeSelected(cubeContent: CubeContent | null, index: number) {
+    this.#selectedCube = cubeContent;
+    const orbitControlsTarget = makeVector3(this.orbitControls.target);
+    if (!cubeContent) {
+      this.#tweenOrbitControls(orbitControlsTarget!, new Vector3(0, 2, 0));
+      return;
+    }
+    const mesh = this.cubes[index];
+    if (mesh) {
+      const target: Vector3 = new Vector3();
+      mesh.getWorldPosition(target);
+      this.#tweenOrbitControls(orbitControlsTarget!, target);
+    }
+  }
+
+  #tweenOrbitControls(source: Vector3, target: Vector3) {
     this.ngZone.runOutsideAngular(() => {
       gsap.to(source, {
         duration: 1,
@@ -245,7 +230,7 @@ export class OrbitControlsTargetSwitcherComponent implements OnInit {
     });
   }
 
-  getRandomColor(): any {
+  #getRandomColor() {
     return niceColors[Math.floor(Math.random() * niceColors.length)];
   }
 }
@@ -261,14 +246,13 @@ export class OrbitControlsTargetSwitcherComponent implements OnInit {
     SimpleCubeComponentModule,
     NgtSobaOrbitControlsModule,
     NgtSpotLightModule,
-    NgtSpotLightHelperModule,
     NgtBoxGeometryModule,
     NgtMeshModule,
     NgtMeshStandardMaterialModule,
     CommonModule,
     NgtMeshPhongMaterialModule,
-    NgtPolarGridHelperModule,
     NgtGridHelperModule,
+    NgtSobaTextModule,
   ],
 })
 export class OrbitControlsTargetSwitcherComponentModule {}
