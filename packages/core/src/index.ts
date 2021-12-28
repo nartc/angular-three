30e84e95ac3e0f4e3a91c127cc6cda4f3b1ea68b
Chau Tran <nartc7789@gmail.com>
2021-12-28 14:14:40 -0600

    feat(core): add Side pipe

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -13,6 +13,7 @@ export * from './lib/pipes/vector.pipe';
 export * from './lib/pipes/color.pipe';
 export * from './lib/pipes/math.pipe';
 export * from './lib/pipes/fog.pipe';
+export * from './lib/pipes/side.pipe';
 
 export * from './lib/directives/repeat.directive';
 export * from './lib/directives/cursor.directive';
