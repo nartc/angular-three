9d894a7251a8d3c9fc5f84f6300c0d67868a637f
Chau Tran <nartc7789@gmail.com>
2021-12-28 12:10:17 -0600

    feat(core): type safe ready for Line

--- packages/core/src/lib/three/line.ts
+++ packages/core/src/lib/three/line.ts
@@ -1,4 +1,10 @@
-import { Directive, Inject, Optional } from '@angular/core';
+import {
+  Directive,
+  EventEmitter,
+  Inject,
+  Optional,
+  Output,
+} from '@angular/core';
 import * as THREE from 'three';
 import {
   NGT_MATERIAL_GEOMETRY_WATCHED_CONTROLLER,
@@ -10,11 +16,19 @@ import {
   providers: [{ provide: NGT_OBJECT_TYPE, useValue: THREE.Line }],
 })
 export abstract class NgtCommonLine<TLine extends THREE.Line = THREE.Line> {
+  @Output() ready = new EventEmitter<TLine>();
+
   constructor(
     @Optional()
     @Inject(NGT_MATERIAL_GEOMETRY_WATCHED_CONTROLLER)
-    protected materialGeometryController: NgtMaterialGeometryController | null
-  ) {}
+    protected materialGeometryController: NgtMaterialGeometryController
+  ) {
+    if (materialGeometryController) {
+      materialGeometryController.readyFn = () => {
+        this.ready.emit(this.line);
+      };
+    }
+  }
 
   get line() {
     return this.materialGeometryController?.objectController.object3d as TLine;
