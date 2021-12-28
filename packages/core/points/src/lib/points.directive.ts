ec84c883b712f14405ea6c8a4eeb11a527ec3df9
Chau Tran <nartc7789@gmail.com>
2021-12-28 12:09:35 -0600

    feat(core): type safe ready for Points

--- packages/core/points/src/lib/points.directive.ts
+++ packages/core/points/src/lib/points.directive.ts
@@ -5,7 +5,13 @@ import {
   NgtMaterialGeometryController,
   NgtMaterialGeometryControllerModule,
 } from '@angular-three/core';
-import { Directive, Inject, NgModule } from '@angular/core';
+import {
+  Directive,
+  EventEmitter,
+  Inject,
+  NgModule,
+  Output,
+} from '@angular/core';
 import * as THREE from 'three';
 
 @Directive({
@@ -17,10 +23,16 @@ import * as THREE from 'three';
   ],
 })
 export class NgtPoints {
+  @Output() ready = new EventEmitter<THREE.Points>();
+
   constructor(
     @Inject(NGT_MATERIAL_GEOMETRY_WATCHED_CONTROLLER)
     private materialGeometryController: NgtMaterialGeometryController
-  ) {}
+  ) {
+    materialGeometryController.readyFn = () => {
+      this.ready.emit(this.points);
+    };
+  }
 
   get points() {
     return this.materialGeometryController.objectController
