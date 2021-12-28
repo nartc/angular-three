26f1a41d249ece76fe75ad43fbd0f207d03692cb
Chau Tran <nartc7789@gmail.com>
2021-12-28 12:10:26 -0600

    feat(core): type safe ready for Mesh

--- packages/core/src/lib/three/mesh.ts
+++ packages/core/src/lib/three/mesh.ts
@@ -1,4 +1,11 @@
-import { Directive, Inject, NgZone, Optional } from '@angular/core';
+import {
+  Directive,
+  EventEmitter,
+  Inject,
+  NgZone,
+  Optional,
+  Output,
+} from '@angular/core';
 import * as THREE from 'three';
 import {
   NGT_MATERIAL_GEOMETRY_WATCHED_CONTROLLER,
@@ -7,12 +14,20 @@ import {
 
 @Directive()
 export abstract class NgtCommonMesh<TMesh extends THREE.Mesh = THREE.Mesh> {
+  @Output() ready = new EventEmitter<TMesh>();
+
   constructor(
     @Optional()
     @Inject(NGT_MATERIAL_GEOMETRY_WATCHED_CONTROLLER)
     protected materialGeometryController: NgtMaterialGeometryController,
     protected ngZone: NgZone
-  ) {}
+  ) {
+    if (materialGeometryController) {
+      materialGeometryController.readyFn = () => {
+        this.ready.emit(this.mesh);
+      };
+    }
+  }
 
   get mesh() {
     return this.materialGeometryController.objectController.object3d as TMesh;
