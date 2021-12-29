d2832d463831dc43f803aa6146cb7583683ec09d
Chau Tran <nartc7789@gmail.com>
2021-12-29 11:24:54 -0600

    docs(demo): use simple cube

--- packages/demo/src/app/app.component.ts
+++ packages/demo/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngt-root',
   template: `
-    <!--    <ngt-simple-cube></ngt-simple-cube>-->
+    <ngt-simple-cube></ngt-simple-cube>
     <!--    <ngt-minecraft></ngt-minecraft>-->
     <!--    <ngt-keen-bloom></ngt-keen-bloom>-->
     <!--    <ngt-instances></ngt-instances>-->
-    <ngt-level-of-details></ngt-level-of-details>
+    <!--    <ngt-level-of-details></ngt-level-of-details>-->
     <!--    <ngt-robot-expressive></ngt-robot-expressive>-->
     <!--    <ngt-orbit-controls-target-switcher></ngt-orbit-controls-target-switcher>-->
     <!--    <ngt-routing></ngt-routing>-->
