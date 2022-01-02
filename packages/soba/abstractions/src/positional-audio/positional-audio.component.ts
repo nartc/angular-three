9044253b39d42fea285d0dd71b40747f5319f29e
Chau Tran <nartc7789@gmail.com>
2022-01-01 19:52:37 -0600

    fix(soba): clean up positional audio

--- packages/soba/abstractions/src/positional-audio/positional-audio.component.ts
+++ packages/soba/abstractions/src/positional-audio/positional-audio.component.ts
@@ -15,7 +15,6 @@ import {
   ChangeDetectionStrategy,
   Component,
   Inject,
-  Injectable,
   Input,
   NgModule,
 } from '@angular/core';
@@ -38,25 +37,66 @@ export interface NgtSobaPositionalAudioState {
   autoplay: boolean;
 }
 
-@Injectable()
-export class NgtSobaPositionalAudioStore extends EnhancedRxState<NgtSobaPositionalAudioState> {
-  readonly buffer$ = this.select('url').pipe(
-    switchMap((url) => this.loaderService.use(THREE.AudioLoader, url))
-  );
+@Component({
+  selector: 'ngt-soba-positional-audio',
+  template: `
+    <ngt-positional-audio
+      *ngIf="listener$ | async as listener"
+      (ready)="onPositionalAudioReady($event)"
+      [listener]="listener"
+      [audioController]="audioController"
+    ></ngt-positional-audio>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    NGT_AUDIO_CONTROLLER_PROVIDER,
+    EnhancedRxState,
+    { provide: NgtSobaExtender, useExisting: NgtSobaPositionalAudio },
+  ],
+})
+export class NgtSobaPositionalAudio extends NgtSobaExtender<THREE.PositionalAudio> {
+  @Input() set url(url: string) {
+    this.state.set({ url });
+  }
 
+  @Input() set distance(distance: number) {
+    this.state.set({ distance });
+  }
+
+  @Input() set loop(loop: boolean) {
+    this.state.set({ loop });
+  }
+
+  readonly listener$ = this.state.select('listener');
+  readonly buffer$ = this.state
+    .select('url')
+    .pipe(switchMap((url) => this.loaderService.use(THREE.AudioLoader, url)));
   readonly changes$ = combineLatest([
     this.buffer$,
-    this.select(selectSlice(['distance', 'loop'])),
+    this.state.select(selectSlice(['distance', 'loop'])),
   ]).pipe(map(([buffer, { loop, distance }]) => ({ buffer, loop, distance })));
 
   constructor(
+    @Inject(NGT_AUDIO_WATCHED_CONTROLLER)
+    public audioController: NgtAudioController,
     private loaderService: NgtLoaderService,
-    private store: NgtStore
+    private store: NgtStore,
+    private state: EnhancedRxState<NgtSobaPositionalAudioState>
   ) {
     super();
-    this.set({ listener: new THREE.AudioListener(), distance: 1, loop: true });
-    this.hold(this.changes$, ({ buffer, loop, distance }) => {
-      const { autoplay, audio } = this.get();
+    state.set({ listener: new THREE.AudioListener(), distance: 1, loop: true });
+
+    state.connect(
+      'autoplay',
+      audioController.change$.pipe(
+        map(() => audioController.autoplay || false),
+        startWith(audioController.autoplay || false),
+        distinctUntilChanged()
+      )
+    );
+
+    state.hold(this.changes$, ({ buffer, loop, distance }) => {
+      const { autoplay, audio } = state.get();
       if (audio) {
         audio.setBuffer(buffer);
         audio.setRefDistance(distance);
@@ -65,8 +105,8 @@ export class NgtSobaPositionalAudioStore extends EnhancedRxState<NgtSobaPosition
       }
     });
 
-    this.holdEffect(this.select('autoplay'), () => {
-      const { audio, listener } = this.get();
+    state.holdEffect(state.select('autoplay'), () => {
+      const { audio, listener } = state.get();
       const camera = this.store.get('camera');
       if (camera) {
         camera.add(listener);
@@ -88,59 +128,10 @@ export class NgtSobaPositionalAudioStore extends EnhancedRxState<NgtSobaPosition
       };
     });
   }
-}
-
-@Component({
-  selector: 'ngt-soba-positional-audio',
-  template: `
-    <ngt-positional-audio
-      *ngIf="listener$ | async as listener"
-      (ready)="onPositionalAudioReady($event)"
-      [listener]="listener"
-      [audioController]="audioController"
-    ></ngt-positional-audio>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    NGT_AUDIO_CONTROLLER_PROVIDER,
-    NgtSobaPositionalAudioStore,
-    { provide: NgtSobaExtender, useExisting: NgtSobaPositionalAudio },
-  ],
-})
-export class NgtSobaPositionalAudio extends NgtSobaExtender<THREE.PositionalAudio> {
-  @Input() set url(url: string) {
-    this.sobaPositionalAudioStore.set({ url });
-  }
-
-  @Input() set distance(distance: number) {
-    this.sobaPositionalAudioStore.set({ distance });
-  }
-
-  @Input() set loop(loop: boolean) {
-    this.sobaPositionalAudioStore.set({ loop });
-  }
-
-  readonly listener$ = this.sobaPositionalAudioStore.select('listener');
-
-  constructor(
-    @Inject(NGT_AUDIO_WATCHED_CONTROLLER)
-    public audioController: NgtAudioController,
-    private sobaPositionalAudioStore: NgtSobaPositionalAudioStore
-  ) {
-    super();
-    sobaPositionalAudioStore.connect(
-      'autoplay',
-      audioController.change$.pipe(
-        map(() => audioController.autoplay || false),
-        startWith(audioController.autoplay || false),
-        distinctUntilChanged()
-      )
-    );
-  }
 
   onPositionalAudioReady(audio: THREE.PositionalAudio) {
     this.object = audio;
-    this.sobaPositionalAudioStore.set({ audio });
+    this.state.set({ audio });
   }
 }
 
