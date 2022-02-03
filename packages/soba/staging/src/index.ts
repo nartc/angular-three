12a5840ad8e44baa5aabd68dd881b235ff4e48b2
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-02 21:58:23 -0600

    chore(repo): upgrade nx

--- packages/soba/staging/src/index.ts
+++ packages/soba/staging/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/backdrop/backdrop';
 export * from './lib/bounds/bounds';
 export * from './lib/camera-shake/camera-shake';
 export * from './lib/center/center';
+export * from './lib/environment/environment';
+export * from './lib/environment/presets';
