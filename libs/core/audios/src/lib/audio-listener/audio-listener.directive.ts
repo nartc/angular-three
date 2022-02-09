56cb6e82724c2d835bcc9a128042441c066f3213
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-09 16:03:27 -0600

    feat(core/audios): add AudioListener

---
+++ libs/core/audios/src/lib/audio-listener/audio-listener.directive.ts
@@ -0,0 +1,68 @@
+import {
+  applyProps,
+  NgtCanvasStore,
+  NgtStore,
+  tapEffect,
+} from '@angular-three/core';
+import {
+  Directive,
+  EventEmitter,
+  Input,
+  NgModule,
+  NgZone,
+  OnInit,
+  Output,
+} from '@angular/core';
+import * as THREE from 'three';
+
+@Directive({
+  selector: 'ngt-audio-listener',
+  exportAs: 'ngtAudioListener',
+})
+export class NgtAudioListener extends NgtStore implements OnInit {
+  @Input() filter?: AudioNode;
+  @Input() timeDelta?: number;
+
+  @Output() ready = new EventEmitter<THREE.AudioListener>();
+
+  private _listener!: THREE.AudioListener;
+  get listener() {
+    return this._listener;
+  }
+
+  constructor(private zone: NgZone, private canvasStore: NgtCanvasStore) {
+    super();
+  }
+
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {
+      this.effect<boolean>(
+        tapEffect(() => {
+          this._listener = new THREE.AudioListener();
+          applyProps(this.listener, {
+            filter: this.filter,
+            timeDelta: this.timeDelta,
+          });
+          const camera = this.canvasStore.get((s) => s.camera);
+          if (camera) {
+            camera.add(this.listener);
+            this.ready.emit(this.listener);
+          }
+
+          return () => {
+            if (camera && this.listener) {
+              this.listener.clear();
+              camera.remove(this.listener);
+            }
+          };
+        })
+      )(this.canvasStore.ready$);
+    });
+  }
+}
+
+@NgModule({
+  declarations: [NgtAudioListener],
+  exports: [NgtAudioListener],
+})
+export class NgtAudioListenerModule {}
