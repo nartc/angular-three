786a0e3247c1b01343200ffc8d2ad1daa78bb5af
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-10 11:31:55 -0600

    feat(soba/controls): add TransformControls

--- libs/soba/controls/src/index.ts
+++ libs/soba/controls/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/orbit-controls/orbit-controls';
 export * from './lib/first-person-controls/first-person-controls';
 export * from './lib/fly-controls/fly-controls';
+export * from './lib/transform-controls/transform-controls';
