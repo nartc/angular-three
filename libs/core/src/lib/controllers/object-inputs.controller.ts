2b4510a57ad6d62ac0b753620a43473bb207ead0
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-10 11:31:29 -0600

    chore(core): add comment to object inputs controller for copy/paste

--- libs/core/src/lib/controllers/object-inputs.controller.ts
+++ libs/core/src/lib/controllers/object-inputs.controller.ts
@@ -187,3 +187,35 @@ export const [
     watchedControllerTokenName: 'Watched ObjectInputsController',
     controller: NgtObjectInputsController,
 });
+
+/**
+ * Used to assign on other object
+ * [name]="objectInputsController.name"
+ * [position]="objectInputsController.position"
+ * [rotation]="objectInputsController.rotation"
+ * [quaternion]="objectInputsController.quaternion"
+ * [scale]="objectInputsController.scale"
+ * [color]="objectInputsController.color"
+ * [userData]="objectInputsController.userData"
+ * [castShadow]="objectInputsController.castShadow"
+ * [receiveShadow]="objectInputsController.receiveShadow"
+ * [visible]="objectInputsController.visible"
+ * [matrixAutoUpdate]="objectInputsController.matrixAutoUpdate"
+ * [dispose]="objectInputsController.dispose"
+ * [raycast]="objectInputsController.raycast"
+ * [appendMode]="objectInputsController.appendMode"
+ * [appendTo]="objectInputsController.appendTo"
+ * (click)="objectInputsController.click.emit($event)"
+ * (contextmenu)="objectInputsController.contextmenu.emit($event)"
+ * (dblclick)="objectInputsController.dblclick.emit($event)"
+ * (pointerup)="objectInputsController.pointerup.emit($event)"
+ * (pointerdown)="objectInputsController.pointerdown.emit($event)"
+ * (pointerover)="objectInputsController.pointerover.emit($event)"
+ * (pointerout)="objectInputsController.pointerout.emit($event)"
+ * (pointerenter)="objectInputsController.pointerenter.emit($event)"
+ * (pointerleave)="objectInputsController.pointerleave.emit($event)"
+ * (pointermove)="objectInputsController.pointermove.emit($event)"
+ * (pointermissed)="objectInputsController.pointermissed.emit($event)"
+ * (pointercancel)="objectInputsController.pointercancel.emit($event)"
+ * (wheel)="objectInputsController.wheel.emit($event)"
+ */
