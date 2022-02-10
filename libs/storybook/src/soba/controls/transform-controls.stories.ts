cac7be9d565df75f8d4a0fd5714c7327090901e0
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-10 13:42:36 -0600

    docs(storybook): add TransformControls stories

--- libs/storybook/src/soba/controls/transform-controls.stories.ts
+++ libs/storybook/src/soba/controls/transform-controls.stories.ts
@@ -1,54 +1,54 @@
+import { provideCanvasOptions } from '@angular-three/core';
+import { NgtBoxGeometryModule } from '@angular-three/core/geometries';
 import { NgtMeshBasicMaterialModule } from '@angular-three/core/materials';
-import { NgtSobaBoxModule } from '@angular-three/soba/shapes';
-import { setupCanvas, setupCanvasModules } from '@angular-three/storybook';
+import { NgtMeshModule } from '@angular-three/core/meshes';
+import { NgtSobaTransformControlsModule } from '@angular-three/soba/controls';
 import {
-  componentWrapperDecorator,
-  Meta,
-  moduleMetadata,
-  Story,
+    componentWrapperDecorator,
+    Meta,
+    moduleMetadata,
+    Story,
 } from '@storybook/angular';
-import {
-  NgtSobaTransformControls,
-  NgtSobaTransformControlsModule,
-} from './transform-controls.component';
+import { setupCanvas, setupCanvasModules } from '../../setup-canvas';
 
 export default {
-  title: 'Soba/Controls/Transform Controls',
-  component: NgtSobaTransformControls,
+    title: 'Soba/Controls/Transform Controls',
 } as Meta;
 
 export const Default: Story = () => ({
-  template: `
-    <ngt-soba-transform-controls>
-      <ngt-soba-box>
-        <ngt-mesh-basic-material [parameters]='{wireframe: true}'></ngt-mesh-basic-material>
-      </ngt-soba-box>
-    </ngt-soba-transform-controls>
+    template: `
+        <ngt-soba-transform-controls>
+          <ngt-mesh>
+            <ngt-box-geometry></ngt-box-geometry>
+            <ngt-mesh-basic-material [parameters]="{wireframe: true}"></ngt-mesh-basic-material>
+          </ngt-mesh>
+        </ngt-soba-transform-controls>
   `,
 });
 Default.decorators = [
-  componentWrapperDecorator(setupCanvas({ black: true })),
-  moduleMetadata({
-    imports: [
-      ...setupCanvasModules,
-      NgtSobaTransformControlsModule,
-      NgtSobaBoxModule,
-      NgtMeshBasicMaterialModule,
-    ],
-  }),
+    componentWrapperDecorator(setupCanvas()),
+    moduleMetadata({
+        imports: [
+            ...setupCanvasModules,
+            NgtSobaTransformControlsModule,
+            NgtMeshModule,
+            NgtBoxGeometryModule,
+            NgtMeshBasicMaterialModule,
+        ],
+        providers: [provideCanvasOptions({ initialLog: true })],
+    }),
 ];
 
 export const LockOrbitControls: Story = Default.bind({});
 LockOrbitControls.decorators = [
-  componentWrapperDecorator(
-    setupCanvas({ black: true, makeControlsDefault: true })
-  ),
-  moduleMetadata({
-    imports: [
-      ...setupCanvasModules,
-      NgtSobaTransformControlsModule,
-      NgtSobaBoxModule,
-      NgtMeshBasicMaterialModule,
-    ],
-  }),
+    componentWrapperDecorator(setupCanvas({ makeControlsDefault: true })),
+    moduleMetadata({
+        imports: [
+            ...setupCanvasModules,
+            NgtSobaTransformControlsModule,
+            NgtMeshModule,
+            NgtBoxGeometryModule,
+            NgtMeshBasicMaterialModule,
+        ],
+    }),
 ];
