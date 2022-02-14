c8d1c49de58a356893e7f9da8ce235c7a0751465
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-14 14:38:23 -0600

    docs(storybook): update docs

--- apps/demo/src/app/simple-cube/simple-cube.component.ts
+++ apps/demo/src/app/simple-cube/simple-cube.component.ts
@@ -1,6 +1,10 @@
-import { NgtCoreModule } from '@angular-three/core';
+import { NgtCoreModule, NgtVector3 } from '@angular-three/core';
 import { NgtBoxGeometryModule } from '@angular-three/core/geometries';
-import { NgtBoxHelperModule } from '@angular-three/core/helpers';
+import { NgtGroupModule } from '@angular-three/core/group';
+import {
+    NgtBoxHelperModule,
+    NgtGridHelperModule,
+} from '@angular-three/core/helpers';
 import {
     NgtAmbientLightModule,
     NgtSpotLightModule,
@@ -9,30 +13,32 @@ import { NgtMeshStandardMaterialModule } from '@angular-three/core/materials';
 import { NgtMeshModule } from '@angular-three/core/meshes';
 import { NgtStatsModule } from '@angular-three/core/stats';
 import { NgtSobaOrbitControlsModule } from '@angular-three/soba/controls';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    NgModule,
+} from '@angular/core';
 import { Object3D } from 'three';
 
 @Component({
     selector: 'demo-simple-cube',
     template: `
-        <ngt-canvas>
+        <ngt-canvas [camera]="{ position: [5, 5, 5] }">
             <ngt-ambient-light></ngt-ambient-light>
             <ngt-spot-light [position]="[5, 5, 5]"></ngt-spot-light>
 
-            <ngt-mesh
-                [ngtBoxHelper]="['black']"
-                (animateReady)="onCubeAnimate($event.object)"
-                (click)="active = !active"
-                (pointerover)="hover = true"
-                (pointerout)="hover = false"
-                [scale]="active ? [1.5, 1.5, 1.5] : [1, 1, 1]"
+            <ngt-group
+                [ngtBoxHelper]="['red']"
+                (animateReady)="onGroupAnimate($event.object)"
             >
-                <ngt-box-geometry></ngt-box-geometry>
-                <ngt-mesh-standard-material
-                    [parameters]="{ color: hover ? 'hotpink' : 'orange' }"
-                ></ngt-mesh-standard-material>
-            </ngt-mesh>
+                <demo-cube [position]="[2, 0, 0]"></demo-cube>
+                <demo-cube [position]="[-2, 0, 0]"></demo-cube>
+                <demo-cube [position]="[0, 0, 2]"></demo-cube>
+                <demo-cube [position]="[0, 0, -2]"></demo-cube>
+            </ngt-group>
 
+            <ngt-grid-helper></ngt-grid-helper>
             <ngt-soba-orbit-controls></ngt-soba-orbit-controls>
 
             <ngt-stats></ngt-stats>
@@ -41,6 +47,34 @@ import { Object3D } from 'three';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SimpleCubeComponent {
+    onGroupAnimate(group: Object3D) {
+        group.rotation.y += 0.01;
+    }
+}
+
+@Component({
+    selector: 'demo-cube',
+    template: `
+        <ngt-mesh
+            [ngtBoxHelper]="['black']"
+            (animateReady)="onCubeAnimate($event.object)"
+            (click)="active = !active"
+            (pointerover)="hover = true"
+            (pointerout)="hover = false"
+            [scale]="active ? [1.5, 1.5, 1.5] : [1, 1, 1]"
+            [position]="position"
+        >
+            <ngt-box-geometry></ngt-box-geometry>
+            <ngt-mesh-standard-material
+                [parameters]="{ color: hover ? 'hotpink' : 'orange' }"
+            ></ngt-mesh-standard-material>
+        </ngt-mesh>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CubeComponent {
+    @Input() position?: NgtVector3;
+
     hover = false;
     active = false;
 
@@ -50,7 +84,7 @@ export class SimpleCubeComponent {
 }
 
 @NgModule({
-    declarations: [SimpleCubeComponent],
+    declarations: [SimpleCubeComponent, CubeComponent],
     exports: [SimpleCubeComponent],
     imports: [
         NgtCoreModule,
@@ -62,6 +96,8 @@ export class SimpleCubeComponent {
         NgtStatsModule,
         NgtMeshModule,
         NgtSobaOrbitControlsModule,
+        NgtGridHelperModule,
+        NgtGroupModule,
     ],
 })
 export class SimpleCubeComponentModule {}
