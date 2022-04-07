f420839a32093d652db600556be96a8f07d95d2e
Chau Tran <nartc7789@gmail.com>
2022-04-07 14:51:47 -0500

    feat(core): adjust LOD

--- libs/core/lod/src/lib/lod.ts
+++ libs/core/lod/src/lib/lod.ts
@@ -1,52 +1,79 @@
-import {
-    createParentObjectProvider,
-    NGT_OBJECT_CONTROLLER_PROVIDER,
-    NGT_OBJECT_WATCHED_CONTROLLER,
-    NgtObjectController,
-    NgtObjectControllerModule,
-} from '@angular-three/core';
-import {
-    Directive,
-    EventEmitter,
-    Inject,
-    NgModule,
-    OnInit,
-    Output,
-} from '@angular/core';
+import { NgtObject, provideObjectFactory } from '@angular-three/core';
+import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import * as THREE from 'three';
 
-@Directive({
+@Component({
     selector: 'ngt-lod',
-    exportAs: 'ngtLod',
-    providers: [
-        NGT_OBJECT_CONTROLLER_PROVIDER,
-        createParentObjectProvider(NgtLod, (lod) => lod.lod),
-    ],
+    template: '<ng-content></ng-content>',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [provideObjectFactory<THREE.LOD>(NgtLod)],
 })
-export class NgtLod implements OnInit {
-    @Output() ready = new EventEmitter<THREE.LOD>();
-
-    private _lod?: THREE.LOD;
-
-    get lod() {
-        return this._lod as THREE.LOD;
+export class NgtLod extends NgtObject<THREE.LOD> {
+    protected override objectInitFn(): THREE.LOD {
+        return new THREE.LOD();
     }
 
-    constructor(
-        @Inject(NGT_OBJECT_WATCHED_CONTROLLER)
-        private objectController: NgtObjectController
-    ) {
-        objectController.initFn = () => (this._lod = new THREE.LOD());
-        objectController.readyFn = () => this.ready.emit(this.lod);
-    }
-
-    ngOnInit() {
-        this.objectController.init();
+    override ngOnInit() {
+        this.init();
+        super.ngOnInit();
     }
 }
 
 @NgModule({
     declarations: [NgtLod],
-    exports: [NgtLod, NgtObjectControllerModule],
+    exports: [NgtLod],
 })
 export class NgtLodModule {}
+
+// import {
+//     createParentObjectProvider,
+//     NGT_OBJECT_CONTROLLER_PROVIDER,
+//     NGT_OBJECT_WATCHED_CONTROLLER,
+//     NgtObjectController,
+//     NgtObjectControllerModule,
+// } from '@angular-three/core';
+// import {
+//     Directive,
+//     EventEmitter,
+//     Inject,
+//     NgModule,
+//     OnInit,
+//     Output,
+// } from '@angular/core';
+// import * as THREE from 'three';
+//
+// @Directive({
+//     selector: 'ngt-lod',
+//     exportAs: 'ngtLod',
+//     providers: [
+//         NGT_OBJECT_CONTROLLER_PROVIDER,
+//         createParentObjectProvider(NgtLod, (lod) => lod.lod),
+//     ],
+// })
+// export class NgtLod implements OnInit {
+//     @Output() ready = new EventEmitter<THREE.LOD>();
+//
+//     private _lod?: THREE.LOD;
+//
+//     get lod() {
+//         return this._lod as THREE.LOD;
+//     }
+//
+//     constructor(
+//         @Inject(NGT_OBJECT_WATCHED_CONTROLLER)
+//         private objectController: NgtObjectController
+//     ) {
+//         objectController.initFn = () => (this._lod = new THREE.LOD());
+//         objectController.readyFn = () => this.ready.emit(this.lod);
+//     }
+//
+//     ngOnInit() {
+//         this.objectController.init();
+//     }
+// }
+//
+// @NgModule({
+//     declarations: [NgtLod],
+//     exports: [NgtLod, NgtObjectControllerModule],
+// })
+// export class NgtLodModule {}
