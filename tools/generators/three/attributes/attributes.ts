80fc7251e7685d54d5bebb54533196e410d8dcee
Chau Tran <nartc7789@gmail.com>
2022-04-07 16:37:09 -0500

    chore(repo): adjust attributes generator

--- tools/generators/three/attributes/attributes.ts
+++ tools/generators/three/attributes/attributes.ts
@@ -1,71 +1,139 @@
 import { librarySecondaryEntryPointGenerator } from '@nrwl/angular/generators';
 import {
-  formatFiles,
-  generateFiles,
-  getWorkspaceLayout,
-  logger,
-  names,
-  Tree,
+    formatFiles,
+    generateFiles,
+    getWorkspaceLayout,
+    logger,
+    names,
+    Tree,
 } from '@nrwl/devkit';
 import { join } from 'path';
 import * as THREE from 'three';
 
 export const attributes = [
-  THREE.BufferAttribute.name,
-  THREE.InstancedBufferAttribute.name,
-  THREE.InterleavedBufferAttribute.name,
-  THREE.Float16BufferAttribute.name,
-  THREE.Float32BufferAttribute.name,
-  THREE.Float64BufferAttribute.name,
-  THREE.Int8BufferAttribute.name,
-  THREE.Int16BufferAttribute.name,
-  THREE.Int32BufferAttribute.name,
-  THREE.Uint8BufferAttribute.name,
-  THREE.Uint16BufferAttribute.name,
-  THREE.Uint32BufferAttribute.name,
-  THREE.Uint8ClampedBufferAttribute.name,
+    THREE.BufferAttribute.name,
+    THREE.InstancedBufferAttribute.name,
+    THREE.InterleavedBufferAttribute.name,
+    THREE.Float16BufferAttribute.name,
+    THREE.Float32BufferAttribute.name,
+    THREE.Float64BufferAttribute.name,
+    THREE.Int8BufferAttribute.name,
+    THREE.Int16BufferAttribute.name,
+    THREE.Int32BufferAttribute.name,
+    THREE.Uint8BufferAttribute.name,
+    THREE.Uint16BufferAttribute.name,
+    THREE.Uint32BufferAttribute.name,
+    THREE.Uint8ClampedBufferAttribute.name,
+];
+
+/**
+ * threePrimitiveAttribute
+ */
+export const primitiveAttributes = [
+    {
+        name: THREE.Color.name,
+        inputType: 'NgtColor',
+        makeFunction: 'makeColor',
+    },
+    {
+        name: THREE.Fog.name,
+        inputType: 'NgtFog',
+        makeFunction: 'make',
+    },
+    {
+        name: THREE.FogExp2.name,
+        inputType: 'NgtFogExp2',
+        makeFunction: 'make',
+    },
+    {
+        name: THREE.Vector2.name,
+        inputType: 'NgtVector2',
+        makeFunction: 'makeVector2',
+    },
+    {
+        name: THREE.Vector3.name,
+        inputType: 'NgtVector3',
+        makeFunction: 'makeVector3',
+    },
+    {
+        name: THREE.Vector4.name,
+        inputType: 'NgtVector4',
+        makeFunction: 'makeVector4',
+    },
+    {
+        name: THREE.Matrix3.name,
+        inputType: 'NgtMatrix3',
+        makeFunction: 'make',
+    },
+    {
+        name: THREE.Matrix4.name,
+        inputType: 'NgtMatrix4',
+        makeFunction: 'make',
+    },
 ];
 
 export default async function attributesGenerator(tree: Tree) {
-  const { libsDir } = getWorkspaceLayout(tree);
-  const attributeDir = join(libsDir, 'core', 'attributes');
+    const { libsDir } = getWorkspaceLayout(tree);
+    const attributeDir = join(libsDir, 'core', 'attributes');
 
-  logger.log('Generating attributes...');
+    logger.log('Generating attributes...');
 
-  if (!tree.exists(attributeDir)) {
-    await librarySecondaryEntryPointGenerator(tree, {
-      name: 'attributes',
-      library: 'core',
-      skipModule: true,
-    });
-  }
+    if (!tree.exists(attributeDir)) {
+        await librarySecondaryEntryPointGenerator(tree, {
+            name: 'attributes',
+            library: 'core',
+            skipModule: true,
+        });
+    }
 
-  const generatedAttributes = [];
-  for (const attribute of attributes) {
-    const normalizedNames = names(attribute);
+    const generatedAttributes = [];
+    for (const attribute of attributes) {
+        const normalizedNames = names(attribute);
+
+        generateFiles(
+            tree,
+            join(__dirname, 'files/lib'),
+            join(attributeDir, 'src', 'lib', normalizedNames.fileName),
+            {
+                ...normalizedNames,
+                tmpl: '',
+            }
+        );
+
+        generatedAttributes.push(normalizedNames.fileName);
+    }
+
+    for (const primitiveAttribute of primitiveAttributes) {
+        const normalizedNames = names(primitiveAttribute.name);
+
+        generateFiles(
+            tree,
+            join(__dirname, 'files/primitive'),
+            join(
+                attributeDir,
+                'src',
+                'lib',
+                `${normalizedNames.fileName}-attribute`
+            ),
+            {
+                ...primitiveAttribute,
+                ...normalizedNames,
+                tmpl: '',
+            }
+        );
+
+        generatedAttributes.push(`${normalizedNames.fileName}-attribute`);
+    }
 
     generateFiles(
-      tree,
-      join(__dirname, 'files/lib'),
-      join(attributeDir, 'src', 'lib', normalizedNames.fileName),
-      {
-        ...normalizedNames,
-        tmpl: '',
-      }
+        tree,
+        join(__dirname, 'files/index'),
+        join(attributeDir, 'src'),
+        {
+            items: generatedAttributes,
+            tmpl: '',
+        }
     );
 
-    generatedAttributes.push(normalizedNames.fileName);
-  }
-
-  generateFiles(
-    tree,
-    join(__dirname, 'files/index'),
-    join(attributeDir, 'src'),
-    {
-      items: generatedAttributes,
-      tmpl: '',
-    }
-  );
-
-  await formatFiles(tree);
+    await formatFiles(tree);
 }
--- tools/generators/three/index.ts
+++ tools/generators/three/index.ts
@@ -1,74 +1,77 @@
 import type { Tree } from '@nrwl/devkit';
 import attributesGenerator from './attributes/attributes';
-import audiosGenerator from './audios/audios';
-import camerasGenerator from './cameras/cameras';
-import cannonBodyControllerGenerator from './cannon-body-controller/cannon-body-controller';
-import cannonConstraintControllerGenerator from './cannon-constraint-controller/cannon-constraint-controller';
-import controllersGenerator from './controllers/controllers';
-import curvesGenerator from './curves/curves';
-import geometriesGenerator from './geometries/geometries';
-import helpersGenerator from './helpers/helpers';
-import lightsGenerator from './lights/lights';
-import linesGenerator from './lines/lines';
-import materialsGenerator from './materials/materials';
-import physicBodiesGenerator from './physic-bodies/physic-bodies';
-import physicConstraintsGenerator from './physic-constraints/physic-constraints';
-import simpleEffectControllerGenerator from './postprocessing/simple-effect-controller';
-import simpleEffectsGenerator from './postprocessing/simple-effects';
-import spritesGenerator from './sprites/sprites';
-import texturesGenerator from './textures/textures';
+
+/**
+ * import audiosGenerator from './audios/audios';
+ * import camerasGenerator from './cameras/cameras';
+ * import cannonBodyControllerGenerator from './cannon-body-controller/cannon-body-controller';
+ * import cannonConstraintControllerGenerator from './cannon-constraint-controller/cannon-constraint-controller';
+ * import controllersGenerator from './controllers/controllers';
+ * import curvesGenerator from './curves/curves';
+ * import geometriesGenerator from './geometries/geometries';
+ * import helpersGenerator from './helpers/helpers';
+ * import lightsGenerator from './lights/lights';
+ * import linesGenerator from './lines/lines';
+ * import materialsGenerator from './materials/materials';
+ * import physicBodiesGenerator from './physic-bodies/physic-bodies';
+ * import physicConstraintsGenerator from './physic-constraints/physic-constraints';
+ * import simpleEffectControllerGenerator from './postprocessing/simple-effect-controller';
+ * import simpleEffectsGenerator from './postprocessing/simple-effects';
+ * import spritesGenerator from './sprites/sprites';
+ * import texturesGenerator from './textures/textures';
+ */
 
 export default async function (tree: Tree) {
     await Promise.all([
-        geometriesGenerator(tree),
-        materialsGenerator(tree),
+        // geometriesGenerator(tree),
+        // materialsGenerator(tree),
         attributesGenerator(tree),
-        curvesGenerator(tree),
-        texturesGenerator(tree),
+        // curvesGenerator(tree),
+        // texturesGenerator(tree),
     ]);
 
-    const [
-        audioSelectors,
-        cameraSelectors,
-        helperSelectors,
-        lightSelectors,
-        lineSelectors,
-        spriteSelectors,
-        simpleEffectSelectors,
-        physicBodySelectors,
-        physicConstraintSelectors,
-    ] = await Promise.all([
-        audiosGenerator(tree),
-        camerasGenerator(tree),
-        helpersGenerator(tree),
-        lightsGenerator(tree),
-        linesGenerator(tree),
-        spritesGenerator(tree),
-        simpleEffectsGenerator(tree),
-        physicBodiesGenerator(tree),
-        physicConstraintsGenerator(tree),
-    ]);
+    // const [
+    //     audioSelectors,
+    //     cameraSelectors,
+    //     helperSelectors,
+    //     lightSelectors,
+    //     lineSelectors,
+    //     spriteSelectors,
+    //     simpleEffectSelectors,
+    //     physicBodySelectors,
+    //     physicConstraintSelectors,
+    // ] = await Promise.all([
+    //     audiosGenerator(tree),
+    //     camerasGenerator(tree),
+    //     helpersGenerator(tree),
+    //     lightsGenerator(tree),
+    //     linesGenerator(tree),
+    //     spritesGenerator(tree),
+    //     simpleEffectsGenerator(tree),
+    //     physicBodiesGenerator(tree),
+    //     physicConstraintsGenerator(tree),
+    // ]);
 
-    await Promise.all([
-        controllersGenerator(
-            tree,
-            [
-                ...lightSelectors,
-                ...helperSelectors,
-                ...spriteSelectors,
-                ...cameraSelectors,
-            ],
-            audioSelectors,
-            lineSelectors
-        ),
-        cannonBodyControllerGenerator(
-            tree,
-            physicBodySelectors.map(({ name }) => name)
-        ),
-        cannonConstraintControllerGenerator(
-            tree,
-            physicConstraintSelectors.map(({ name }) => name)
-        ),
-        simpleEffectControllerGenerator(tree, simpleEffectSelectors),
-    ]);
+    // await Promise.all([
+    //     controllersGenerator(
+    //         tree,
+    //         [
+    //             ...lightSelectors,
+    //             ...helperSelectors,
+    //             ...spriteSelectors,
+    //             ...cameraSelectors,
+    //         ],
+    //         audioSelectors,
+    //         lineSelectors
+    //     ),
+    //     cannonBodyControllerGenerator(
+    //         tree,
+    //         physicBodySelectors.map(({ name }) => name)
+    //     ),
+    //     cannonConstraintControllerGenerator(
+    //         tree,
+    //         physicConstraintSelectors.map(({ name }) => name)
+    //     ),
+    //     simpleEffectControllerGenerator(tree, simpleEffectSelectors),
+    // ]);
 }
