a7a9a105a4c261940a95320e90693a3207fcea98
Chau Tran <nartc7789@gmail.com>
2022-04-08 21:34:30 -0500

    fix(core): adjust cube camera to add inputs

--- libs/core/cameras/src/lib/cube-camera/cube-camera.ts
+++ libs/core/cameras/src/lib/cube-camera/cube-camera.ts
@@ -4,7 +4,12 @@ import {
     provideObjectFactory,
     tapEffect,
 } from '@angular-three/core';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    NgModule,
+} from '@angular/core';
 import * as THREE from 'three';
 
 export interface NgtCubeCameraState extends NgtObjectState<THREE.CubeCamera> {
@@ -25,18 +30,50 @@ export class NgtCubeCamera extends NgtObject<
     THREE.CubeCamera,
     NgtCubeCameraState
 > {
+    @Input() set args(args: ConstructorParameters<typeof THREE.CubeCamera>) {
+        this.set({ args });
+    }
+
+    @Input() set near(near: number) {
+        this.set({ near });
+    }
+
+    @Input() set far(far: number) {
+        this.set({ far });
+    }
+
+    @Input() set renderTarget(renderTarget: THREE.WebGLCubeRenderTarget) {
+        this.set({ renderTarget });
+    }
+
     protected override objectInitFn(): THREE.CubeCamera {
         const args = this.get((s) => s.args);
         return new THREE.CubeCamera(...args);
     }
 
     override ngOnInit() {
+        const args = this.get((s) => s.args);
+        if (args && args.length) {
+            this.set({
+                near: args[0],
+                far: args[1],
+                renderTarget: args[2],
+            });
+        }
         this.effect<NgtCubeCameraState['args']>(
             tapEffect(() => {
                 this.init();
             })
         )(this.select((s) => s.args));
         super.ngOnInit();
+    }
+
+    protected override get subInputs(): Record<string, boolean> {
+        return {
+            near: false,
+            far: false,
+            renderTarget: false,
+        };
     }
 }
 
