289938ad3b182756d965789f5e3f90e467a32708
Chau Tran <nartc7789@gmail.com>
2022-04-14 00:50:46 -0500

    feat(cannon): switch to use service based API for Constraint

--- libs/cannon/constraints/src/index.ts
+++ libs/cannon/constraints/src/index.ts
@@ -1,7 +1,2 @@
 // GENERATED
-export * from './lib/point-to-point/point-to-point';
-export * from './lib/cone-twist/cone-twist';
-export * from './lib/distance/distance';
-export * from './lib/hinge/hinge';
-export * from './lib/lock/lock';
 export * from './lib/constraint';
--- libs/cannon/constraints/src/lib/constraint.ts
+++ libs/cannon/constraints/src/lib/constraint.ts
@@ -1,36 +1,24 @@
 import { NgtPhysicsStore } from '@angular-three/cannon';
 import {
-    AnyConstructor,
-    makeId,
     NgtComponentStore,
     NgtStore,
+    NgtUnknownInstance,
+    Ref,
     tapEffect,
 } from '@angular-three/core';
+import { Injectable, NgZone, Optional } from '@angular/core';
 import {
-    Directive,
-    Input,
-    NgZone,
-    OnInit,
-    Optional,
-    Provider,
-} from '@angular/core';
-import { ConstraintOptns, ConstraintTypes } from '@pmndrs/cannon-worker-api';
-import { tap } from 'rxjs';
+    CannonWorkerAPI,
+    ConeTwistConstraintOpts,
+    ConstraintOptns,
+    ConstraintTypes,
+    DistanceConstraintOpts,
+    HingeConstraintOpts,
+    LockConstraintOpts,
+    PointToPointConstraintOpts,
+} from '@pmndrs/cannon-worker-api';
+import { combineLatest, filter } from 'rxjs';
 import * as THREE from 'three';
-
-export function providePhysicsConstraint<
-    TConstraintType extends 'Hinge' | ConstraintTypes,
-    TConstraintOptions extends ConstraintOptns,
-    TConstraint extends NgtPhysicsConstraint<
-        TConstraintType,
-        TConstraintOptions
-    > = NgtPhysicsConstraint<TConstraintType, TConstraintOptions>
->(constraint: AnyConstructor<TConstraint>): Provider {
-    return {
-        provide: NgtPhysicsConstraint,
-        useExisting: constraint,
-    };
-}
 
 type ConstraintApi = {
     disable: () => void;
@@ -49,123 +37,172 @@ type HingeConstraintApi = {
 type NgtConstraintORHingeApi<T extends 'Hinge' | ConstraintTypes> =
     T extends ConstraintTypes ? ConstraintApi : HingeConstraintApi;
 
-export interface NgtPhysicsConstraintState<
-    TConstraintOptions extends ConstraintOptns
-> {
-    bodies: THREE.Object3D[];
-    options: TConstraintOptions;
-    previous?: () => THREE.Object3D;
+export interface NgtConstraintReturn<T extends 'Hinge' | ConstraintTypes> {
+    bodyA: Ref<THREE.Object3D>;
+    bodyB: Ref<THREE.Object3D>;
+    api: NgtConstraintORHingeApi<T>;
 }
 
-@Directive()
-export abstract class NgtPhysicsConstraint<
-        TConstraintType extends 'Hinge' | ConstraintTypes,
-        TConstraintOptions extends ConstraintOptns
-    >
-    extends NgtComponentStore<NgtPhysicsConstraintState<TConstraintOptions>>
-    implements OnInit
-{
-    abstract get constraintType(): TConstraintType;
-
-    private readonly uuid = makeId();
-
-    @Input() set previous(previous: (() => THREE.Object3D) | undefined) {
-        if (previous) {
-            this.set((state) => ({
-                ...state,
-                previous,
-            }));
-        }
-    }
-
-    @Input() set options(options: TConstraintOptions) {
-        this.set({ options });
-    }
-
+@Injectable()
+export class NgtPhysicConstraint extends NgtComponentStore {
     constructor(
-        protected zone: NgZone,
-        protected store: NgtStore,
-        @Optional() protected physicsStore: NgtPhysicsStore
+        private zone: NgZone,
+        private store: NgtStore,
+        @Optional() private physicsStore: NgtPhysicsStore
     ) {
-        super();
-
         if (!physicsStore) {
             throw new Error(
-                '[ngtCannon***Constraint] directive can only be used inside of <ngt-physics>'
+                'NgtPhysicConstraint must be used inside of <ngt-physics>'
             );
         }
-
-        this.set({ bodies: [], options: {} as TConstraintOptions });
+        super();
     }
 
-    ngOnInit() {
-        this.zone.runOutsideAngular(() => {
-            this.onCanvasReady(this.store.ready$, () => {
-                this.init(this.select());
+    usePointToPointConstraint(
+        bodyA: Ref<THREE.Object3D>,
+        bodyB: Ref<THREE.Object3D>,
+        optns: PointToPointConstraintOpts
+    ) {
+        return this.useConstraint('PointToPoint', bodyA, bodyB, optns);
+    }
+
+    useConeTwistConstraint(
+        bodyA: Ref<THREE.Object3D>,
+        bodyB: Ref<THREE.Object3D>,
+        optns: ConeTwistConstraintOpts
+    ) {
+        return this.useConstraint('ConeTwist', bodyA, bodyB, optns);
+    }
+
+    useDistanceConstraint(
+        bodyA: Ref<THREE.Object3D>,
+        bodyB: Ref<THREE.Object3D>,
+        optns: DistanceConstraintOpts
+    ) {
+        return this.useConstraint('Distance', bodyA, bodyB, optns);
+    }
+
+    useHingeConstraint(
+        bodyA: Ref<THREE.Object3D>,
+        bodyB: Ref<THREE.Object3D>,
+        optns: HingeConstraintOpts
+    ) {
+        return this.useConstraint('Hinge', bodyA, bodyB, optns);
+    }
+
+    useLockConstraint(
+        bodyA: Ref<THREE.Object3D>,
+        bodyB: Ref<THREE.Object3D>,
+        optns: LockConstraintOpts
+    ) {
+        return this.useConstraint('Lock', bodyA, bodyB, optns);
+    }
+
+    private useConstraint<TConstraintType extends 'Hinge' | ConstraintTypes>(
+        type: TConstraintType,
+        bodyA: Ref<THREE.Object3D>,
+        bodyB: Ref<THREE.Object3D>,
+        opts: ConstraintOptns | HingeConstraintOpts = {}
+    ): NgtConstraintReturn<TConstraintType> {
+        return this.zone.runOutsideAngular(() => {
+            const physicsStore = this.physicsStore;
+            const uuid = THREE.MathUtils.generateUUID();
+
+            requestAnimationFrame(() => {
+                this.onCanvasReady(
+                    this.store.ready$,
+                    () => {
+                        this.effect<
+                            [CannonWorkerAPI, THREE.Object3D, THREE.Object3D]
+                        >(
+                            tapEffect(([worker, a, b]) => {
+                                worker.addConstraint({
+                                    props: [a.uuid, b.uuid, opts],
+                                    type,
+                                    uuid,
+                                });
+                                return () => worker.removeConstraint({ uuid });
+                            })
+                        )(
+                            combineLatest([
+                                physicsStore.select((s) => s.worker),
+                                bodyA.ref$.pipe(
+                                    filter(
+                                        (ref) =>
+                                            (
+                                                ref as unknown as NgtUnknownInstance
+                                            )['__ngt__'] != undefined
+                                    )
+                                ),
+                                bodyB.ref$.pipe(
+                                    filter(
+                                        (ref) =>
+                                            (
+                                                ref as unknown as NgtUnknownInstance
+                                            )['__ngt__'] != undefined
+                                    )
+                                ),
+                            ])
+                        );
+                    },
+                    true
+                );
             });
+
+            return {
+                bodyA,
+                bodyB,
+                get api() {
+                    const worker = physicsStore.get((s) => s.worker);
+
+                    const enableDisable = {
+                        disable: () => {
+                            requestAnimationFrame(() => {
+                                worker.disableConstraint({ uuid });
+                            });
+                        },
+                        enable: () => {
+                            requestAnimationFrame(() => {
+                                worker.enableConstraint({ uuid });
+                            });
+                        },
+                    } as NgtConstraintORHingeApi<TConstraintType>;
+
+                    if (type === 'Hinge') {
+                        return {
+                            ...enableDisable,
+                            disableMotor: () => {
+                                requestAnimationFrame(() => {
+                                    worker.disableConstraintMotor({ uuid });
+                                });
+                            },
+                            enableMotor: () => {
+                                requestAnimationFrame(() => {
+                                    worker.enableConstraintMotor({ uuid });
+                                });
+                            },
+                            setMotorMaxForce: (value: number) => {
+                                requestAnimationFrame(() => {
+                                    worker.setConstraintMotorMaxForce({
+                                        props: value,
+                                        uuid,
+                                    });
+                                });
+                            },
+                            setMotorSpeed: (value: number) => {
+                                requestAnimationFrame(() => {
+                                    worker.setConstraintMotorSpeed({
+                                        props: value,
+                                        uuid,
+                                    });
+                                });
+                            },
+                        };
+                    }
+
+                    return enableDisable;
+                },
+            };
         });
     }
-
-    readonly addBody = this.effect<THREE.Object3D>(
-        tap((body) => {
-            this.set((state) => {
-                if (state.previous) {
-                    return { ...state, bodies: [body, state.previous()] };
-                }
-
-                return { ...state, bodies: [...state.bodies, body] };
-            });
-        })
-    );
-
-    get api(): NgtConstraintORHingeApi<TConstraintType> {
-        const worker = this.physicsStore.get((s) => s.worker);
-
-        const enableDisable = {
-            disable: () => worker.disableConstraint({ uuid: this.uuid }),
-            enable: () => worker.enableConstraint({ uuid: this.uuid }),
-        } as NgtConstraintORHingeApi<TConstraintType>;
-
-        if (this.constraintType === 'Hinge') {
-            return {
-                ...enableDisable,
-                disableMotor: () =>
-                    worker.disableConstraintMotor({ uuid: this.uuid }),
-                enableMotor: () =>
-                    worker.enableConstraintMotor({ uuid: this.uuid }),
-                setMotorMaxForce: (value: number) =>
-                    worker.setConstraintMotorMaxForce({
-                        props: value,
-                        uuid: this.uuid,
-                    }),
-                setMotorSpeed: (value: number) =>
-                    worker.setConstraintMotorSpeed({
-                        props: value,
-                        uuid: this.uuid,
-                    }),
-            };
-        }
-
-        return enableDisable;
-    }
-
-    private readonly init = this.effect<
-        NgtPhysicsConstraintState<TConstraintOptions>
-    >(
-        tapEffect(({ options, bodies }) => {
-            const worker = this.physicsStore.get((s) => s.worker);
-            const [bodyA, bodyB] = bodies;
-
-            if (bodyA && bodyB) {
-                worker.addConstraint({
-                    props: [bodyA.uuid, bodyB.uuid, options],
-                    type: this.constraintType,
-                    uuid: this.uuid,
-                });
-                return () => worker.removeConstraint({ uuid: this.uuid });
-            }
-
-            return;
-        })
-    );
 }
