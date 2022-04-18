992d86f8bf8095b08ad6f4081a6deb21abf99160
Chau Tran <nartc7789@gmail.com>
2022-04-18 15:40:22 -0500

    docs: clean up example

--- apps/sandbox/src/app/app.component.ts
+++ apps/sandbox/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox-root',
     template: `
-        <!-- <sandbox-cubes></sandbox-cubes> -->
-        <!-- <sandbox-physic-cubes></sandbox-physic-cubes> -->
+        <!--        <sandbox-cubes></sandbox-cubes>-->
+        <sandbox-physic-cubes></sandbox-physic-cubes>
         <!--        <sandbox-monday-morning></sandbox-monday-morning>-->
-        <sandbox-kinematic-cube></sandbox-kinematic-cube>
+        <!--                <sandbox-kinematic-cube></sandbox-kinematic-cube>-->
     `,
     styles: [],
 })
