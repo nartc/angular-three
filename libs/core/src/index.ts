edce7275bcea1de87063b5ed2fead1537695ce13
Chau Tran <nartc7789@gmail.com>
2022-04-20 17:03:35 -0500

    feat(core): add object pass through directive

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,72 +1,72 @@
+/* abstracts */
+export * from './lib/abstracts/instance';
+export * from './lib/abstracts/material-geometry';
+export * from './lib/abstracts/object';
+/* main canvas */
 export * from './lib/canvas';
-export * from './lib/types';
-export * from './lib/tokens';
-export * from './lib/ref';
-
-export * from './lib/di/performance';
-export * from './lib/di/resize';
-export * from './lib/di/is-webgl-available';
-export * from './lib/di/window';
-export * from './lib/di/object';
-export * from './lib/di/material-geometry';
-export * from './lib/di/mesh';
-export * from './lib/di/instance';
+/* dependency injection tokens */
 export * from './lib/di/attribute';
 export * from './lib/di/audio';
 export * from './lib/di/camera';
-export * from './lib/di/geometry';
-export * from './lib/di/material';
 export * from './lib/di/curve';
+export * from './lib/di/geometry';
 export * from './lib/di/helper';
-export * from './lib/di/object-helper';
-export * from './lib/di/line';
+export * from './lib/di/instance';
+export * from './lib/di/is-webgl-available';
 export * from './lib/di/light';
+export * from './lib/di/line';
+export * from './lib/di/material';
+export * from './lib/di/material-geometry';
+export * from './lib/di/mesh';
+export * from './lib/di/object';
+export * from './lib/di/object-helper';
+export * from './lib/di/performance';
+export * from './lib/di/resize';
+export * from './lib/di/skinned-mesh';
 export * from './lib/di/sprite';
 export * from './lib/di/texture';
-export * from './lib/di/skinned-mesh';
-
-export * from './lib/pipes/side';
+export * from './lib/di/window';
+/* directives */
+export * from './lib/directives/cursor';
+export * from './lib/directives/repeat';
+export * from './lib/directives/object-pass-through';
+/* pipes */
+export * from './lib/pipes/color';
+export * from './lib/pipes/fog';
 export * from './lib/pipes/math';
 export * from './lib/pipes/pi';
 export * from './lib/pipes/radian';
-
+export * from './lib/pipes/side';
 export * from './lib/pipes/vector';
-export * from './lib/pipes/color';
-export * from './lib/pipes/fog';
-
-export * from './lib/directives/repeat';
-export * from './lib/directives/cursor';
-
-export * from './lib/abstracts/object';
-export * from './lib/abstracts/instance';
-export * from './lib/abstracts/material-geometry';
-
-export * from './lib/three/audio';
-export * from './lib/three/line';
-export * from './lib/three/texture';
-export * from './lib/three/geometry';
-export * from './lib/three/light';
-export * from './lib/three/attribute';
-export * from './lib/three/curve';
-export * from './lib/three/helper';
-export * from './lib/three/material';
-export * from './lib/three/mesh';
-export * from './lib/three/sprite';
-export * from './lib/three/camera';
-// export * from './lib/three/extender';
-export * from './lib/three/object-helper';
-
-export * from './lib/stores/component-store';
-export * from './lib/stores/store';
-
+export * from './lib/ref';
+/* services */
 export * from './lib/services/loader';
 export * from './lib/services/resize';
-
+/* stores*/
+export * from './lib/stores/component-store';
+export * from './lib/stores/store';
+/* three abstract classes */
+export * from './lib/three/attribute';
+export * from './lib/three/audio';
+export * from './lib/three/camera';
+export * from './lib/three/curve';
+export * from './lib/three/geometry';
+export * from './lib/three/helper';
+export * from './lib/three/light';
+export * from './lib/three/line';
+export * from './lib/three/material';
+export * from './lib/three/mesh';
+export * from './lib/three/object-helper';
+export * from './lib/three/sprite';
+export * from './lib/three/texture';
+export * from './lib/tokens';
+export * from './lib/types';
+/* utils */
 export * from './lib/utils/apply-props';
 export * from './lib/utils/build-graph';
-export * from './lib/utils/make';
 export * from './lib/utils/coercion';
 export * from './lib/utils/instance';
-export * from './lib/utils/shader-material';
 export * from './lib/utils/is';
 export * from './lib/utils/loop';
+export * from './lib/utils/make';
+export * from './lib/utils/shader-material';
---
+++ libs/core/src/lib/directives/object-pass-through.ts
@@ -0,0 +1,149 @@
+import { takeUntil } from 'rxjs';
+import { NgtObject, NgtObjectInputs } from '../abstracts/object';
+import { Directive, Input, NgModule, Optional, Self } from '@angular/core';
+
+@Directive({
+    selector:
+        '[ngtObjectOutputs],[ngtObjectInputs],[ngtObjectInputs][ngtObjectOutputs]',
+})
+export class NgtObjectPassThrough {
+    @Input() set ngtObjectOutputs(wrapper: unknown) {
+        this.assertWrapper(wrapper);
+
+        if (wrapper.click.observed) {
+            this.host.click
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.click.emit.bind(wrapper.click));
+        }
+
+        if (wrapper.contextmenu.observed) {
+            this.host.contextmenu
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.contextmenu.emit.bind(wrapper.contextmenu));
+        }
+
+        if (wrapper.dblclick.observed) {
+            this.host.dblclick
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.dblclick.emit.bind(wrapper.dblclick));
+        }
+
+        if (wrapper.pointerup.observed) {
+            this.host.pointerup
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.pointerup.emit.bind(wrapper.pointerup));
+        }
+
+        if (wrapper.pointerdown.observed) {
+            this.host.pointerdown
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.pointerdown.emit.bind(wrapper.pointerdown));
+        }
+
+        if (wrapper.pointerover.observed) {
+            this.host.pointerover
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.pointerover.emit.bind(wrapper.pointerover));
+        }
+
+        if (wrapper.pointerout.observed) {
+            this.host.pointerout
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.pointerout.emit.bind(wrapper.pointerout));
+        }
+
+        if (wrapper.pointerenter.observed) {
+            this.host.pointerenter
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(
+                    wrapper.pointerenter.emit.bind(wrapper.pointerenter)
+                );
+        }
+
+        if (wrapper.pointerleave.observed) {
+            this.host.pointerleave
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(
+                    wrapper.pointerleave.emit.bind(wrapper.pointerleave)
+                );
+        }
+
+        if (wrapper.pointermove.observed) {
+            this.host.pointermove
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.pointermove.emit.bind(wrapper.pointermove));
+        }
+
+        if (wrapper.pointermissed.observed) {
+            this.host.pointermissed
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(
+                    wrapper.pointermissed.emit.bind(wrapper.pointermissed)
+                );
+        }
+
+        if (wrapper.pointercancel.observed) {
+            this.host.pointercancel
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(
+                    wrapper.pointercancel.emit.bind(wrapper.pointercancel)
+                );
+        }
+
+        if (wrapper.wheel.observed) {
+            this.host.wheel
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.wheel.emit.bind(wrapper.wheel));
+        }
+
+        if (wrapper.ready.observed) {
+            this.host.ready
+                .pipe(takeUntil(wrapper.destroy$))
+                .subscribe(wrapper.ready.emit.bind(wrapper.ready));
+        }
+    }
+
+    @Input() set ngtObjectInputs(wrapper: unknown) {
+        this.assertWrapper(wrapper);
+
+        this.host.ref = wrapper.instance;
+        this.host.attach = wrapper.attach;
+        this.host.skipParent = wrapper.skipParent;
+        this.host.noAttach = wrapper.noAttach;
+        this.host.name = wrapper.name;
+        this.host.position = wrapper.position;
+        this.host.rotation = wrapper.rotation;
+        this.host.quaternion = wrapper.quaternion;
+        this.host.scale = wrapper.scale;
+        this.host.color = wrapper.color;
+        this.host.userData = wrapper.userData;
+        this.host.castShadow = wrapper.castShadow;
+        this.host.receiveShadow = wrapper.receiveShadow;
+        this.host.visible = wrapper.visible;
+        this.host.matrixAutoUpdate = wrapper.matrixAutoUpdate;
+        this.host.dispose = wrapper.dispose;
+        this.host.raycast = wrapper.raycast;
+        this.host.appendMode = wrapper.appendMode;
+        this.host.appendTo = wrapper.appendTo;
+    }
+
+    constructor(@Optional() @Self() private host: NgtObject) {
+        if (!host) return;
+    }
+
+    private assertWrapper(
+        wrapper: unknown
+    ): asserts wrapper is NgtObjectInputs {
+        if (!(wrapper instanceof NgtObjectInputs)) {
+            throw new Error(
+                `[NgtObjectPassThrough] wrapper is not an NgtObject`
+            );
+        }
+    }
+}
+
+@NgModule({
+    declarations: [NgtObjectPassThrough],
+    exports: [NgtObjectPassThrough],
+})
+export class NgtObjectPassThroughModule {}
