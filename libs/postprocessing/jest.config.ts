5888b2e1bcb0b2d1f565c25befb35978e0271876
Chau Tran <nartc7789@gmail.com>
2022-04-21 22:14:24 -0500

    chore: upgrade Nx 14

--- libs/postprocessing/jest.config.ts
+++ libs/postprocessing/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-    displayName: 'cannon',
-    preset: '../../jest.preset.js',
+    displayName: 'postprocessing',
+
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
         'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
             stringifyContentPathRegex: '\\.(html|svg)$',
         },
     },
-    coverageDirectory: '../../coverage/libs/cannon',
+    coverageDirectory: '../../coverage/libs/postprocessing',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
     },
@@ -18,4 +18,5 @@ module.exports = {
         'jest-preset-angular/build/serializers/ng-snapshot',
         'jest-preset-angular/build/serializers/html-comment',
     ],
+    preset: '../../jest.preset.ts',
 };
--- libs/schematics/jest.config.ts
+++ libs/schematics/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
     displayName: 'schematics',
-    preset: '../../jest.preset.js',
+
     globals: {
         'ts-jest': {
             tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,4 +12,5 @@ module.exports = {
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     coverageDirectory: '../../coverage/libs/schematics',
+    preset: '../../jest.preset.ts',
 };
