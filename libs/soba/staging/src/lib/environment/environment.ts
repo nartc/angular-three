3363bf3f4ddb7828f6f9c2826c95a26247ba35e4
Chau Tran <nartc7789@gmail.com>
2022-04-26 17:26:11 -0500

    wip: environment

--- libs/soba/staging/src/lib/environment/environment.ts
+++ libs/soba/staging/src/lib/environment/environment.ts
@@ -1,4 +1,463 @@
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+    AnyFunction,
+    BooleanInput,
+    coerceBooleanProperty,
+    coerceNumberProperty,
+    is,
+    NGT_INSTANCE_HOST_REF,
+    NGT_INSTANCE_REF,
+    NgtComponentStore,
+    NgtInstance,
+    NgtInstanceState,
+    NgtLoader,
+    NgtStore,
+    NumberInput,
+    Ref,
+    startWithUndefined,
+    tapEffect,
+} from '@angular-three/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Directive,
+    Inject,
+    Injectable,
+    Input,
+    NgModule,
+    NgZone,
+    Optional,
+    Self,
+    SkipSelf,
+} from '@angular/core';
+import { isObservable, Observable, of, Subscription } from 'rxjs';
+import * as THREE from 'three';
+import { RGBELoader } from 'three-stdlib';
+import { presetsObj, PresetsType } from './presets';
+
+const CUBEMAP_ROOT =
+    'https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/';
+
+function resolveScene(scene: THREE.Scene | Ref<THREE.Scene>): THREE.Scene {
+    return is.ref(scene) ? scene.value : scene;
+}
+
+/**
+ * frames?: number
+ *   near?: number
+ *   far?: number
+ *   resolution?: number
+ *   background?: boolean | 'only'
+ *   map?: THREE.Texture
+ *   files?: string | string[]
+ *   path?: string
+ *   preset?: PresetsType
+ *   scene?: Scene | React.MutableRefObject<THREE.Scene>
+ *   extensions?: (loader: Loader) => void
+ *   ground?:
+ *     | boolean
+ *     | {
+ *         radius?: number
+ *         height?: number
+ *         scale?: number
+ *       }
+ */
+
+export interface NgtSobaEnvironmentGenericState<T extends object = {}>
+    extends NgtInstanceState<T> {
+    frames?: number;
+    near?: number;
+    far?: number;
+    resolution?: number;
+    background?: boolean | 'only';
+    map?: THREE.Texture;
+    files?: string | string[];
+    path?: string;
+    preset?: PresetsType;
+    scene?: THREE.Scene | Ref<THREE.Scene>;
+    extensions?: (loader: THREE.Loader) => void;
+    ground?:
+        | boolean
+        | {
+              radius?: number;
+              height?: number;
+              scale?: number;
+          };
+}
+
+@Directive()
+export abstract class NgtSobaEnvironmentGeneric<
+    T extends object = {}
+> extends NgtInstance<T, NgtSobaEnvironmentGenericState<T>> {
+    get frames() {
+        return this.get((s) => s.frames);
+    }
+    @Input() set frames(frames: NumberInput) {
+        this.set({ frames: coerceNumberProperty(frames) });
+    }
+
+    get near() {
+        return this.get((s) => s.near);
+    }
+    @Input() set near(near: NumberInput) {
+        this.set({ near: coerceNumberProperty(near) });
+    }
+
+    get far() {
+        return this.get((s) => s.far);
+    }
+    @Input() set far(far: NumberInput) {
+        this.set({ far: coerceNumberProperty(far) });
+    }
+
+    get resolution() {
+        return this.get((s) => s.resolution);
+    }
+    @Input() set resolution(resolution: NumberInput) {
+        this.set({ resolution: coerceNumberProperty(resolution) });
+    }
+
+    get background() {
+        return this.get((s) => s.background);
+    }
+    @Input() set background(background: BooleanInput | 'only') {
+        this.set({
+            background:
+                background === 'only'
+                    ? background
+                    : coerceBooleanProperty(background),
+        });
+    }
+
+    get map() {
+        return this.get((s) => s.map) as THREE.Texture;
+    }
+    @Input() set map(map: THREE.Texture) {
+        this.set({ map });
+    }
+
+    get files() {
+        return this.get((s) => s.files) as string | string[];
+    }
+    @Input() set files(files: string | string[]) {
+        this.set({ files });
+    }
+
+    get path() {
+        return this.get((s) => s.path) as string;
+    }
+    @Input() set path(path: string) {
+        this.set({ path });
+    }
+
+    get preset() {
+        return this.get((s) => s.preset) as PresetsType;
+    }
+    @Input() set preset(preset: PresetsType) {
+        this.set({ preset });
+    }
+
+    get scene() {
+        return this.get((s) => s.scene) as THREE.Scene | Ref<THREE.Scene>;
+    }
+    @Input() set scene(scene: THREE.Scene | Ref<THREE.Scene>) {
+        this.set({ scene });
+    }
+
+    get extensions() {
+        return this.get((s) => s.extensions) as (loader: THREE.Loader) => void;
+    }
+    @Input() set extensions(extensions: (loader: THREE.Loader) => void) {
+        this.set({ extensions });
+    }
+
+    get ground() {
+        return this.get((s) => s.ground);
+    }
+    @Input() set ground(
+        ground:
+            | BooleanInput
+            | {
+                  radius?: number;
+                  height?: number;
+                  scale?: number;
+              }
+    ) {
+        this.set({
+            ground: is.obj(ground) ? ground : coerceBooleanProperty(ground),
+        });
+    }
+}
+
+@Directive({
+    selector: '[ngtSobaEnvironmentPassThrough]',
+})
+export class NgtSobaEnvironmentPassThrough {
+    @Input() set ngtSobaEnvironmentPassThrough(wrapper: unknown) {
+        this.assertWrapper(wrapper);
+
+        this.host.frames = wrapper.frames;
+        this.host.near = wrapper.near;
+        this.host.far = wrapper.far;
+        this.host.resolution = wrapper.resolution;
+        this.host.background = wrapper.background;
+        this.host.map = wrapper.map;
+        this.host.files = wrapper.files;
+        this.host.path = wrapper.path;
+        this.host.preset = wrapper.preset;
+        this.host.scene = wrapper.scene;
+        this.host.extensions = wrapper.extensions;
+        this.host.ground = wrapper.ground;
+    }
+
+    constructor(@Self() @Optional() private host: NgtSobaEnvironmentGeneric) {
+        if (!host) return;
+    }
+
+    private assertWrapper(
+        wrapper: unknown
+    ): asserts wrapper is NgtSobaEnvironmentGeneric {
+        if (!(wrapper instanceof NgtSobaEnvironmentGeneric)) {
+            throw new Error(
+                `[ngtSobaEnvironmentPassThrough] wrapper is not an NgtSobaEnvironmentGeneric`
+            );
+        }
+    }
+}
+
+export type NgtSobaEnvironmentResolverParams = Partial<
+    Pick<
+        NgtSobaEnvironmentGenericState,
+        'files' | 'path' | 'preset' | 'extensions'
+    >
+>;
+
+export interface NgtSobaEnvironmentResolverState
+    extends Pick<
+        NgtSobaEnvironmentGenericState,
+        'files' | 'path' | 'preset' | 'extensions'
+    > {
+    textureRef: Ref<THREE.Texture | THREE.CubeTexture>;
+}
+
+@Injectable()
+export class NgtSobaEnvironmentResolver extends NgtComponentStore<NgtSobaEnvironmentResolverState> {
+    constructor(private loader: NgtLoader, private store: NgtStore) {
+        super();
+        this.set({ textureRef: new Ref() });
+    }
+
+    private useSubscription?: Subscription;
+
+    use(
+        paramsFactory: (
+            defaultParams: NgtSobaEnvironmentResolverParams
+        ) =>
+            | NgtSobaEnvironmentResolverParams
+            | Observable<NgtSobaEnvironmentResolverParams>
+    ): Ref<THREE.Texture | THREE.CubeTexture> {
+        if (this.useSubscription) {
+            this.useSubscription.unsubscribe();
+        }
+
+        const params = paramsFactory({
+            files: [
+                '/px.png',
+                '/nx.png',
+                '/py.png',
+                '/ny.png',
+                '/pz.png',
+                '/nz.png',
+            ],
+            path: '',
+        });
+
+        const params$ = isObservable(params) ? params : of(params);
+
+        this.set(params$);
+
+        this.useSubscription = this.onCanvasReady(
+            this.store.ready$,
+            () => {
+                this.setTexture(
+                    this.select(
+                        this.select((s) => s.path),
+                        this.select((s) => s.files),
+                        this.select((s) => s.preset).pipe(startWithUndefined()),
+                        this.select((s) => s.extensions).pipe(
+                            startWithUndefined()
+                        )
+                    )
+                );
+
+                return () => {
+                    if (this.useSubscription) {
+                        this.useSubscription.unsubscribe();
+                    }
+                };
+            },
+            true
+        );
+
+        return this.get((s) => s.textureRef);
+    }
+
+    private readonly setTexture = this.effect<{}>(
+        tapEffect(() => {
+            const { textureRef, extensions, preset } = this.get();
+            let { files, path } = this.get();
+
+            if (preset) {
+                if (!(preset in presetsObj))
+                    throw new Error(
+                        'Preset must be one of: ' +
+                            Object.keys(presetsObj).join(', ')
+                    );
+                files = presetsObj[preset];
+                path = CUBEMAP_ROOT;
+            }
+
+            const isCubeMap = Array.isArray(files);
+            const loader = isCubeMap ? THREE.CubeTextureLoader : RGBELoader;
+
+            const sub = this.loader
+                .use(
+                    // @ts-expect-error
+                    loader,
+                    isCubeMap ? [files] : files,
+                    (loader) => {
+                        loader.setPath(path!);
+                        if (extensions) extensions(loader);
+                    }
+                ) // @ts-ignore
+                .subscribe((data) => {
+                    const texture: THREE.Texture | THREE.CubeTexture = isCubeMap
+                        ? // @ts-ignore
+                          data[0]
+                        : data;
+
+                    texture.mapping = isCubeMap
+                        ? THREE.CubeReflectionMapping
+                        : THREE.EquirectangularReflectionMapping;
+
+                    textureRef.set(texture);
+                });
+
+            return () => {
+                sub.unsubscribe();
+            };
+        })
+    );
+}
+
+@Component({
+    selector: 'ngt-soba-environment-map[map]',
+    template: `<ng-content></ng-content>`,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        {
+            provide: NgtSobaEnvironmentGeneric,
+            useExisting: NgtSobaEnvironmentMap,
+        },
+    ],
+})
+export class NgtSobaEnvironmentMap extends NgtSobaEnvironmentGeneric {
+    protected override preInit() {
+        super.preInit();
+        this.set((state) => ({
+            background: state.background ?? false,
+        }));
+    }
+
+    override ngOnInit() {
+        super.ngOnInit();
+        this.zone.runOutsideAngular(() => {
+            this.onCanvasReady(this.store.ready$, () => {
+                this.setEnvironment(
+                    this.select(
+                        this.store.select((s) => s.scene),
+                        this.select((s) => s.background),
+                        this.select((s) => s.map),
+                        this.select((s) => s.scene).pipe(startWithUndefined())
+                    )
+                );
+            });
+        });
+    }
+
+    private readonly setEnvironment = this.effect<{}>(
+        tapEffect(() => {
+            const { map, background, scene } = this.get();
+            const defaultScene = this.store.get((s) => s.scene);
+
+            if (map) {
+                const target = resolveScene(scene || defaultScene);
+                const oldBg = target.background;
+                const oldEnv = target.environment;
+
+                if (background !== 'only') {
+                    target.environment = map;
+                }
+
+                if (background) {
+                    target.background = map;
+                }
+
+                return () => {
+                    if (background !== 'only') {
+                        target.environment = oldEnv;
+                    }
+
+                    if (background) {
+                        target.background = oldBg;
+                    }
+                };
+            }
+            return;
+        })
+    );
+}
+
+@Component({
+    selector: 'ngt-soba-environment-cube',
+    template: ``,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        {
+            provide: NgtSobaEnvironmentGeneric,
+            useExisting: NgtSobaEnvironmentCube,
+        },
+        NgtSobaEnvironmentResolver,
+    ],
+})
+export class NgtSobaEnvironmentCube extends NgtSobaEnvironmentGeneric {
+    constructor(
+        zone: NgZone,
+        store: NgtStore,
+        @Optional()
+        @SkipSelf()
+        @Inject(NGT_INSTANCE_REF)
+        parentRef: AnyFunction<Ref>,
+        @Optional()
+        @SkipSelf()
+        @Inject(NGT_INSTANCE_HOST_REF)
+        parentHostRef: AnyFunction<Ref>,
+        private environmentResolver: NgtSobaEnvironmentResolver
+    ) {
+        super(zone, store, parentRef, parentHostRef);
+    }
+
+    protected override preInit() {
+        super.preInit();
+        this.set((state) => ({
+            background: state.background ?? false,
+        }));
+    }
+
+    override ngOnInit() {
+        super.ngOnInit();
+        // this.zone.runOutsideAngular(() => {});
+    }
+}
 
 @Component({
     selector: 'ngt-soba-environment',
@@ -12,7 +471,12 @@ export class NgtSobaEnvironment {
 }
 
 @NgModule({
-    declarations: [NgtSobaEnvironment],
+    declarations: [
+        NgtSobaEnvironment,
+        NgtSobaEnvironmentPassThrough,
+        NgtSobaEnvironmentMap,
+        NgtSobaEnvironmentCube,
+    ],
     exports: [NgtSobaEnvironment],
 })
 export class NgtSobaEnvironmentModule {}
