97282aaa08e76ae498bf7333f74c1964e9ee25aa
Chau Tran <nartc7789@gmail.com>
2022-04-25 21:28:45 -0500

    chore(core): remove all factory tokens

--- libs/core/src/lib/tokens.ts
+++ libs/core/src/lib/tokens.ts
@@ -1,110 +1,41 @@
 import { InjectionToken } from '@angular/core';
 
-export const NGT_INSTANCE_FACTORY = new InjectionToken('NgtInstance Factory');
 export const NGT_INSTANCE_REF = new InjectionToken('NgtInstance ref');
-
 export const NGT_INSTANCE_HOST_REF = new InjectionToken(
     'NgtInstance ref of the host of the wrapper'
 );
-
-export const NGT_OBJECT_FACTORY = new InjectionToken('NgtObject factory');
 export const NGT_OBJECT_REF = new InjectionToken('NgtObject ref');
 export const NGT_OBJECT_HOST_REF = new InjectionToken(
     'NgtObject ref of the host of the wrapper'
 );
-
-export const NGT_CAMERA_INSTANCE_FACTORY = new InjectionToken(
-    'NgtObject factory for root Camera'
-);
 export const NGT_CAMERA_REF = new InjectionToken('Ref for root Camera');
-
-export const NGT_SCENE_INSTANCE_FACTORY = new InjectionToken(
-    'NgtObject factory for root Scene'
-);
 export const NGT_SCENE_REF = new InjectionToken('Ref for root Scene');
-
-export const NGT_COMMON_ATTRIBUTE_FACTORY = new InjectionToken(
-    'NgtCommonAttribute factory'
-);
 export const NGT_COMMON_ATTRIBUTE_REF = new InjectionToken(
     'NgtCommonAttribute ref'
 );
-
-export const NGT_COMMON_AUDIO_FACTORY = new InjectionToken(
-    'NgtCommonAudio factory'
-);
 export const NGT_COMMON_AUDIO_REF = new InjectionToken('NgtCommonAudio ref');
-
-export const NGT_COMMON_CAMERA_FACTORY = new InjectionToken(
-    'NgtCommonCamera factory'
-);
 export const NGT_COMMON_CAMERA_REF = new InjectionToken('NgtCommonCamera ref');
-
-export const NGT_COMMON_CURVE_FACTORY = new InjectionToken(
-    'NgtCommonCurve factory'
-);
 export const NGT_COMMON_CURVE_REF = new InjectionToken('NgtCommonCurve ref');
-
-export const NGT_COMMON_GEOMETRY_FACTORY = new InjectionToken(
-    'NgtCommonGeometry factory'
-);
 export const NGT_COMMON_GEOMETRY_REF = new InjectionToken(
     'NgtCommonGeometry ref'
 );
-
-export const NGT_COMMON_HELPER_FACTORY = new InjectionToken(
-    'NgtCommonHelper factory'
-);
 export const NGT_COMMON_HELPER_REF = new InjectionToken('NgtCommonHelper ref');
-
-export const NGT_COMMON_LIGHT_FACTORY = new InjectionToken(
-    'NgtCommonLight factory'
-);
 export const NGT_COMMON_LIGHT_REF = new InjectionToken('NgtCommonLight ref');
-
-export const NGT_COMMON_LINE_FACTORY = new InjectionToken(
-    'NgtCommonLine factory'
-);
 export const NGT_COMMON_LINE_REF = new InjectionToken('NgtCommonLine ref');
-
-export const NGT_COMMON_MATERIAL_FACTORY = new InjectionToken(
-    'NgtCommonMaterial factory'
-);
 export const NGT_COMMON_MATERIAL_REF = new InjectionToken(
     'NgtCommonMaterial ref'
-);
-
-export const NGT_MATERIAL_GEOMETRY_OBJECT_FACTORY = new InjectionToken(
-    'NgtMaterialGeometry factory'
 );
 export const NGT_MATERIAL_GEOMETRY_OBJECT_REF = new InjectionToken(
     'NgtMaterialGeometry ref'
 );
-
-export const NGT_COMMON_MESH_FACTORY = new InjectionToken(
-    'NgtCommonMesh factory'
-);
 export const NGT_COMMON_MESH_REF = new InjectionToken('NgtCommonMesh ref');
-
-export const NGT_COMMON_OBJECT_HELPER_FACTORY = new InjectionToken(
-    'NgtCommonObjectHelper factory'
-);
 export const NGT_COMMON_OBJECT_HELPER_REF = new InjectionToken(
     'NgtCommonObjectHelper ref'
 );
-
-export const NGT_COMMON_SPRITE_FACTORY = new InjectionToken(
-    'NgtCommonSprite factory'
-);
 export const NGT_COMMON_SPRITE_REF = new InjectionToken('NgtCommonSprite ref');
-
-export const NGT_COMMON_TEXTURE_FACTORY = new InjectionToken(
-    'NgtCommonTexture factory'
-);
 export const NGT_COMMON_TEXTURE_REF = new InjectionToken(
     'NgtCommonTexture ref'
 );
-
 export const NGT_HOST_SKINNED_MESH_REF = new InjectionToken(
     'NgtSkinnedMesh ref as parent host'
 );
