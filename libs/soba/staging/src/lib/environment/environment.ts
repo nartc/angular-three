ca2d6ec6294beee5b3608f913bd29177c5b679d2
Chau Tran <nartc7789@gmail.com>
2022-04-27 08:15:09 -0500

    fix(soba): use inline glsl for now

--- libs/soba/staging/src/lib/environment/environment.ts
+++ libs/soba/staging/src/lib/environment/environment.ts
@@ -1,7 +1,3 @@
-// @ts-ignore
-import * as fragmentShader from '!!raw-loader!./GroundProjection.frag.glsl';
-// @ts-ignore
-import * as vertexShader from '!!raw-loader!./GroundProjection.vert.glsl';
 import {
     AnyFunction,
     BooleanInput,
@@ -60,6 +56,89 @@ import {
 import * as THREE from 'three';
 import { RGBELoader } from 'three-stdlib';
 import { presetsObj, PresetsType } from './presets';
+
+const fragmentShader =
+    // language=glsl
+    `
+#define ENVMAP_TYPE_CUBE_UV
+
+varying vec3 vWorldPosition;
+
+uniform float radius;
+uniform float height;
+
+#ifdef ENVMAP_TYPE_CUBE
+  uniform samplerCube cubemap;
+#else
+  uniform sampler2D cubemap;
+#endif
+
+// From: https://www.iquilezles.org/www/articles/intersectors/intersectors.htm
+float diskIntersect( in vec3 ro, in vec3 rd, vec3 c, vec3 n, float r ) {
+    vec3  o = ro - c;
+    float t = -dot(n, o)/dot(rd,n);
+    vec3  q = o + rd * t;
+    return (dot(q, q) < r * r) ? t : 1e6;
+}
+
+// From: https://www.iquilezles.org/www/articles/intersectors/intersectors.htm
+float sphereIntersect( in vec3 ro, in vec3 rd, in vec3 ce, float ra ) {
+    vec3 oc = ro - ce;
+    float b = dot( oc, rd );
+    float c = dot( oc, oc ) - ra*ra;
+    float h = b * b - c;
+    if(h < 0.0) -1.0;
+    h = sqrt( h );
+    return -b + h;
+}
+
+vec3 project() {
+  vec3 p = normalize(vWorldPosition);
+  vec3 camPos = cameraPosition;
+  camPos.y -= height;
+
+  float intersection = sphereIntersect(camPos, p, vec3(0.), radius);
+  if(intersection > 0.) {
+    vec3 h = vec3(0.0,-height,0.0);
+    float intersection2 = diskIntersect(camPos, p, h, vec3(0.0,-1.0,0.0), radius);
+    p = (camPos + min(intersection, intersection2) * p) / radius;
+  }
+  else {
+    p = vec3(0.0,1.0,0.0);
+  }
+  return p;
+}
+
+#include <common>
+#include <cube_uv_reflection_fragment>
+
+void main(){
+  vec3 projectedWorldPosition = project();
+
+  #ifdef ENVMAP_TYPE_CUBE
+    vec3 outcolor = textureCube(cubemap, projectedWorldPosition).rgb;
+  #else
+    vec3 direction = normalize(projectedWorldPosition);
+    vec2 uv = equirectUv(direction);
+    vec3 outcolor = texture2D(cubemap, uv).rgb;
+  #endif
+
+  gl_FragColor = vec4(outcolor, 1.0);
+
+  #include <tonemapping_fragment>
+  #include <encodings_fragment>
+}`;
+
+const vertexShader =
+    // language=glsl
+    `
+varying vec3 vWorldPosition;
+
+void main() {
+  vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+  vWorldPosition = worldPosition.xyz;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}`;
 
 const CUBEMAP_ROOT =
     'https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/';
@@ -733,7 +812,7 @@ export class NgtSobaEnvironmentGround extends NgtSobaEnvironmentGeneric {
         super(zone, store, parentRef, parentHostRef);
     }
 
-    readonly vertexShader = vertexShader.default;
+    readonly vertexShader = vertexShader;
 
     readonly uniforms = {
         cubemap: { value: null },
@@ -838,7 +917,7 @@ export class NgtSobaEnvironmentGround extends NgtSobaEnvironmentGeneric {
     private readonly setFragment = this.effect<{}>(
         tap(() => {
             const defines = this.get((s) => s['defines']);
-            this.set({ fragment: defines.join('\n') + fragmentShader.default });
+            this.set({ fragment: defines.join('\n') + fragmentShader });
         })
     );
 
