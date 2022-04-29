f85cea15724e4a7c96b75b3f827efe537d8d0474
Chau Tran <nartc7789@gmail.com>
2022-04-29 00:03:48 -0500

    chore: remove storybook

--- libs/storybook/.storybook/theme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-    base: 'light',
-    brandTitle: 'Angular Three',
-    brandUrl: 'https://github.com/nartc/angular-three',
-    brandImage: 'https://angular-three.netlify.com/ngt-logo.svg',
-});
--- libs/storybook/README.md
+++
@@ -1,3 +0,0 @@
-# storybook
-
-This library was generated with [Nx](https://nx.dev).
