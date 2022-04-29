10bb6dd7d60b7b50d162ad4fea9a483f53fdbbf6
Chau Tran <nartc7789@gmail.com>
2022-04-28 22:47:24 -0500

    feat(soba): add CameraShake

---
+++ libs/soba/src/staging/camera-shake.stories.ts
@@ -0,0 +1,135 @@
+import {
+    NgtMathPipeModule,
+    NgtRadianPipeModule,
+    NgtSidePipeModule,
+} from '@angular-three/core';
+import { NgtValueAttributeModule } from '@angular-three/core/attributes';
+import {
+    NgtBoxGeometryModule,
+    NgtPlaneGeometryModule,
+} from '@angular-three/core/geometries';
+import { NgtMeshStandardMaterialModule } from '@angular-three/core/materials';
+import { NgtMeshModule } from '@angular-three/core/meshes';
+import { NgtSobaCameraShakeModule } from '@angular-three/soba/staging';
+import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+    componentWrapperDecorator,
+    Meta,
+    moduleMetadata,
+    Story,
+} from '@storybook/angular';
+import * as THREE from 'three';
+import { setupCanvas, setupCanvasModules } from '../setup-canvas';
+
+@Component({
+    selector: 'ngt-default-camera-shake-scene',
+    template: `
+        <ngt-mesh (beforeRender)="onBeforeRender($event.object)">
+            <ngt-box-geometry [args]="[2, 2, 2]"></ngt-box-geometry>
+            <ngt-mesh-standard-material wireframe></ngt-mesh-standard-material>
+        </ngt-mesh>
+        <ngt-mesh [position]="[0, -6, 0]" [rotation]="[-90 | radian, 0, 0]">
+            <ngt-value-attribute
+                [attach]="['rotation', 'x']"
+                [value]="-90 | radian"
+            ></ngt-value-attribute>
+
+            <ngt-plane-geometry
+                [args]="[200, 200, 75, 75]"
+            ></ngt-plane-geometry>
+            <ngt-mesh-standard-material
+                wireframe
+                color="red"
+                [side]="'double' | side"
+            ></ngt-mesh-standard-material>
+        </ngt-mesh>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+class DefaultCameraShakeScene {
+    onBeforeRender(cube: THREE.Object3D) {
+        cube.rotation.x = cube.rotation.y += 0.01;
+    }
+}
+
+@NgModule({
+    declarations: [DefaultCameraShakeScene],
+    exports: [DefaultCameraShakeScene],
+    imports: [
+        NgtBoxGeometryModule,
+        NgtMeshStandardMaterialModule,
+        NgtPlaneGeometryModule,
+        NgtMathPipeModule,
+        NgtSidePipeModule,
+        NgtMeshModule,
+        NgtRadianPipeModule,
+        NgtValueAttributeModule,
+    ],
+})
+class DefaultCameraShakeSceneModule {}
+
+export default {
+    title: 'Staging/Camera Shake',
+    decorators: [
+        componentWrapperDecorator(
+            setupCanvas({ cameraPosition: [0, 0, 10], controls: false })
+        ),
+        moduleMetadata({
+            imports: [
+                ...setupCanvasModules,
+                NgtSobaCameraShakeModule,
+                DefaultCameraShakeSceneModule,
+            ],
+        }),
+    ],
+} as Meta;
+
+export const Default: Story = (args) => ({
+    props: args,
+    template: `
+        <ngt-soba-camera-shake
+            [maxPitch]="maxPitch"
+            [maxRoll]="maxRoll"
+            [maxYaw]="maxYaw"
+            [pitchFrequency]="pitchFrequency"
+            [rollFrequency]="rollFrequency"
+            [yawFrequency]="yawFrequency"
+        ></ngt-soba-camera-shake>
+        <ngt-default-camera-shake-scene></ngt-default-camera-shake-scene>
+    `,
+});
+
+Default.args = {
+    maxPitch: 0.05,
+    maxRoll: 0.05,
+    maxYaw: 0.05,
+    pitchFrequency: 0.8,
+    rollFrequency: 0.8,
+    yawFrequency: 0.8,
+};
+
+export const WithOrbitControls: Story = (args) => ({
+    props: args,
+    template: `
+    <!-- orbit controls needs to be BEFORE camera-shake -->
+    <ngt-soba-orbit-controls makeDefault></ngt-soba-orbit-controls>
+    <ngt-soba-camera-shake
+        [maxPitch]="maxPitch"
+        [maxRoll]="maxRoll"
+        [maxYaw]="maxYaw"
+        [pitchFrequency]="pitchFrequency"
+        [rollFrequency]="rollFrequency"
+        [yawFrequency]="yawFrequency"
+    ></ngt-soba-camera-shake>
+    <ngt-default-camera-shake-scene></ngt-default-camera-shake-scene>
+  `,
+});
+
+WithOrbitControls.args = {
+    maxPitch: 0.05,
+    maxRoll: 0.05,
+    maxYaw: 0.05,
+    pitchFrequency: 0.8,
+    rollFrequency: 0.8,
+    yawFrequency: 0.8,
+};
