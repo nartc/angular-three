a1a2cd214754238e7a69195906337e53b30ecf92
Chau Tran <nartc7789@gmail.com>
2022-05-02 10:37:26 -0500

    docs: adjust keen bloom to use ngt-effect-composer-content

--- apps/sandbox/src/app/keen-bloom/keen-bloom.component.ts
+++ apps/sandbox/src/app/keen-bloom/keen-bloom.component.ts
@@ -16,7 +16,7 @@ import * as THREE from 'three';
 @Component({
   selector: 'sandbox-keen-bloom',
   template: `
-    <ngt-canvas [camera]="{ position: [0, 0, 15], near: 5, far: 20 }">
+    <ngt-canvas initialLog [camera]="{ position: [0, 0, 15], near: 5, far: 20 }">
       <ngt-color attach="background" color="black"></ngt-color>
 
       <ngt-soba-orbit-controls></ngt-soba-orbit-controls>
@@ -26,7 +26,9 @@ import * as THREE from 'three';
       <ngt-directional-light [position]="[0, 1, 2]" color="white"></ngt-directional-light>
 
       <ngt-effect-composer>
-        <ngt-bloom></ngt-bloom>
+        <ng-template ngt-effect-composer-content>
+          <ngt-bloom></ngt-bloom>
+        </ng-template>
       </ngt-effect-composer>
     </ngt-canvas>
     <ngt-soba-loader></ngt-soba-loader>
