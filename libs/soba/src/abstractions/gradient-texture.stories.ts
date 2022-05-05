abce8438423545e4988414f797f5c9a264dec1fa
Adrian Moore <adrianrob@hotmail.com>
2022-05-05 13:05:02 -0700

    Added args/props support to Gradient Texture story

--- libs/soba/src/abstractions/gradient-texture.stories.ts
+++ libs/soba/src/abstractions/gradient-texture.stories.ts
@@ -3,7 +3,7 @@ import { NgtPlaneGeometryModule } from '@angular-three/core/geometries';
 import { NgtMeshBasicMaterialModule } from '@angular-three/core/materials';
 import { NgtMeshModule } from '@angular-three/core/meshes';
 import { NgtSobaGradientTextureModule } from '@angular-three/soba/abstractions';
-import { componentWrapperDecorator, Meta, moduleMetadata } from '@storybook/angular';
+import { componentWrapperDecorator, Meta, moduleMetadata, Story } from '@storybook/angular';
 import { setupCanvas, setupCanvasModules } from '../setup-canvas';
 
 export default {
@@ -23,13 +23,19 @@ export default {
   ],
 } as Meta;
 
-export const Default = () => ({
+export const Default: Story = (args) => ({
+  props: args,
   template: `
         <ngt-mesh>
             <ngt-plane-geometry></ngt-plane-geometry>
             <ngt-mesh-basic-material [depthWrite]="false" [side]="'double' | side">
-                <ngt-soba-gradient-texture [stops]="[0, 1]" [colors]='["aquamarine", "hotpink"]'></ngt-soba-gradient-texture>
+                <ngt-soba-gradient-texture [stops]="stops" [colors]="colors"></ngt-soba-gradient-texture>
             </ngt-mesh-basic-material>
         </ngt-mesh>
     `,
 });
+
+Default.args = {
+    stops: [0, 1],
+    colors: ['aquamarine', 'hotpink'],
+};
