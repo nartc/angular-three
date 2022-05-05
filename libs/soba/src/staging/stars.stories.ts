84529f2f22b68d2041bfad37ef8bd4326b64f9b0
Adrian Moore <adrianrob@hotmail.com>
2022-05-05 16:14:08 -0700

    Added story props/args to match DREI

--- libs/soba/src/staging/stars.stories.ts
+++ libs/soba/src/staging/stars.stories.ts
@@ -4,7 +4,8 @@ import { NgtAxesHelperModule } from '@angular-three/core/helpers';
 import { NgtMeshBasicMaterialModule } from '@angular-three/core/materials';
 import { NgtMeshModule } from '@angular-three/core/meshes';
 import { NgtSobaStars, NgtSobaStarsModule } from '@angular-three/soba/staging';
-import { componentWrapperDecorator, Meta, moduleMetadata } from '@storybook/angular';
+import { componentWrapperDecorator, Meta, moduleMetadata, Story } from '@storybook/angular';
+import { createRangeControl } from '../create-control';
 import { setupCanvas, setupCanvasModules } from '../setup-canvas';
 
 export default {
@@ -26,9 +27,10 @@ export default {
   ],
 } as Meta;
 
-export const Default = () => ({
+export const Default: Story = (args) => ({
+    props: args,
   template: `
-        <ngt-soba-stars></ngt-soba-stars>
+        <ngt-soba-stars [radius]="radius" [count]="count" [factor]="factor" [fade]="fade" [speed]="speed"></ngt-soba-stars>
         <ngt-mesh [rotation]="[90 | radian, 0, 0]">
             <ngt-plane-geometry  [args]="[100, 100, 4, 4]"></ngt-plane-geometry>
             <ngt-mesh-basic-material wireframe></ngt-mesh-basic-material>
@@ -36,3 +38,27 @@ export const Default = () => ({
         <ngt-axes-helper></ngt-axes-helper>
   `,
 });
+
+Default.args = {
+    radius: 100,
+    count: 5000,
+    factor: 4,
+    fade: false,
+    speed: 1,
+
+};
+
+Default.argTypes = {
+    radius: {
+        control: createRangeControl(100, 400, 100),
+    },
+    count: {
+        control: createRangeControl(500, 5000, 500),
+    },
+    factor: {
+        control: createRangeControl(1, 10, 1),
+    },
+    speed: {
+        control: createRangeControl(1, 4, 1),
+    },
+};
