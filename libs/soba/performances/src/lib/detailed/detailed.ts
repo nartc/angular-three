fc150e34a7e93f19c9ee57180e666765446369c0
Chau Tran <nartc7789@gmail.com>
2022-05-06 23:03:32 -0500

    fix(soba): fix ContentChildren; use ref for Detailed

--- libs/soba/performances/src/lib/detailed/detailed.ts
+++ libs/soba/performances/src/lib/detailed/detailed.ts
@@ -10,6 +10,7 @@ import {
 import { NgtLodModule } from '@angular-three/core/lod';
 import { CommonModule } from '@angular/common';
 import {
+  AfterContentInit,
   ChangeDetectionStrategy,
   Component,
   ContentChild,
@@ -20,15 +21,13 @@ import {
   QueryList,
   TemplateRef,
 } from '@angular/core';
-import { asyncScheduler, defer, observeOn, of, pipe, startWith, tap } from 'rxjs';
+import { asyncScheduler, combineLatest, observeOn, pipe, switchMap, tap } from 'rxjs';
 import * as THREE from 'three';
 
 @Directive({
   selector: 'ng-template[ngt-soba-detailed-content]',
 })
 export class NgtSobaDetailedContent {
-  @ContentChildren(NGT_OBJECT_REF) children!: QueryList<AnyFunction<Ref>>;
-
   constructor(public templateRef: TemplateRef<{ lod: Ref<THREE.LOD> }>) {}
 
   static ngTemplateContextGuard(dir: NgtSobaDetailedContent, ctx: any): ctx is { lod: Ref<THREE.LOD> } {
@@ -51,8 +50,9 @@ export class NgtSobaDetailedContent {
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [provideObjectHostRef(NgtSobaDetailed)],
 })
-export class NgtSobaDetailed extends NgtObjectInputs<THREE.LOD> {
+export class NgtSobaDetailed extends NgtObjectInputs<THREE.LOD> implements AfterContentInit {
   @ContentChild(NgtSobaDetailedContent) content?: NgtSobaDetailedContent;
+  @ContentChildren(NGT_OBJECT_REF) children!: QueryList<AnyFunction<Ref>>;
 
   @Input() set distances(distances: number[]) {
     this.set({ distances });
@@ -65,17 +65,14 @@ export class NgtSobaDetailed extends NgtObjectInputs<THREE.LOD> {
     }));
   }
 
-  override ngOnInit() {
-    super.ngOnInit();
+  ngAfterContentInit() {
     this.zone.runOutsideAngular(() => {
       this.onCanvasReady(this.store.ready$, () => {
         this.addLevels(
           this.select(
             this.select((s) => s['distances']),
             this.instance$,
-            defer(() => {
-              return this.content?.children.changes.pipe(startWith(this.content?.children)) || of(null);
-            })
+            this.children.changes.pipe(switchMap(() => combineLatest(this.children.map((child) => child()))))
           )
         );
       });
