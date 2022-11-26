ef41dd3361b3b24fca7651f7ed3c011fcbfedc16
Chau Tran <nartc7789@gmail.com>
2022-11-25 23:10:41 -0600

    chore: gradient texture

--- packages/soba/abstractions/src/index.ts
+++ packages/soba/abstractions/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/text-3d/text-3d';
 export * from './lib/line/line';
 export * from './lib/cubic-bezier-line/cubic-bezier-line';
 export * from './lib/quadratic-bezier-line/quadratic-bezier-line';
+export * from './lib/gradient-texture/gradient-texture';
---
+++ packages/soba/abstractions/src/lib/gradient-texture/gradient-texture.ts
@@ -0,0 +1,44 @@
+import { checkNeedsUpdate, injectArgs, NgtInstance, NgtStore, provideInstanceRef, proxify } from '@angular-three/core';
+import { Component, inject } from '@angular/core';
+import { tap } from 'rxjs';
+import * as THREE from 'three';
+import { NGT_INSTANCE_INPUTS } from '../common';
+
+@Component({
+    selector: 'ngt-soba-gradient-texture',
+    standalone: true,
+    template: '<ng-content></ng-content>',
+    hostDirectives: [{ directive: NgtInstance, inputs: NGT_INSTANCE_INPUTS }],
+    providers: [provideInstanceRef(SobaGradientTexture)],
+})
+export class SobaGradientTexture extends THREE.Texture {
+    constructor() {
+        const store = inject(NgtStore);
+        const [stops, colors, size = 1024] = injectArgs() as [stops: number[], colors: string[], size?: number];
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d')!;
+        canvas.width = 16;
+        canvas.height = size;
+        const gradient = context.createLinearGradient(0, 0, 0, size);
+        let i = stops.length;
+        while (i--) {
+            gradient.addColorStop(stops[i], colors[i]);
+        }
+        context.fillStyle = gradient;
+        context.fillRect(0, 0, 16, size);
+        super(canvas);
+        return proxify(this, {
+            attach: 'map',
+            created: (instance, _, ngtInstance) => {
+                checkNeedsUpdate(instance);
+
+                ngtInstance.effect(
+                    tap(() => {
+                        instance.encoding = store.read((s) => s.gl.outputEncoding);
+                        checkNeedsUpdate(instance);
+                    })
+                )(store.select((s) => s.gl.outputEncoding, { debounce: true }));
+            },
+        });
+    }
+}
