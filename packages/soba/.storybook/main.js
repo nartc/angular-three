468ad53acb4566600694b47d855458af0a87a2cc
Chau Tran <nartc7789@gmail.com>
2022-12-27 20:16:05 -0600

    storybook

---
+++ packages/soba/.storybook/main.js
@@ -0,0 +1,18 @@
+const rootMain = require('../../../.storybook/main');
+
+module.exports = {
+  ...rootMain,
+  core: { ...rootMain.core, builder: 'webpack5' },
+  stories: [...rootMain.stories, '../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [...rootMain.addons],
+  webpackFinal: async (config, { configType }) => {
+    // apply any global webpack configs that might have been specified in .storybook/main.js
+    if (rootMain.webpackFinal) {
+      config = await rootMain.webpackFinal(config, { configType });
+    }
+
+    // add your own webpack tweaks if needed
+
+    return config;
+  },
+};
