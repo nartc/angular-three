d41ecaf2f6d47706d948772c0651a5b0863fad06
Chau Tran <nartc7789@gmail.com>
2022-12-28 11:56:03 -0600

    text is ready

---
+++ packages/soba/abstractions/src/lib/text/text.ts
@@ -0,0 +1,108 @@
+import {
+  injectNgtRef,
+  injectNgtStore,
+  NgtArgs,
+  NgtRendererFlags,
+  NgtRxStore,
+} from '@angular-three/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  EventEmitter,
+  inject,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { RxActionFactory } from '@rx-angular/state/actions';
+// @ts-ignore
+import { preloadFont, Text } from 'troika-three-text';
+
+@Component({
+  selector: 'ngts-text[text]',
+  standalone: true,
+  template: `
+    <ngt-primitive
+      ngtCompound
+      *args="[ref.nativeElement]"
+      [text]="get('text')"
+      [anchorX]="get('anchorX')"
+      [anchorY]="get('anchorY')"
+      [font]="get('font')"
+    >
+      <ng-content></ng-content>
+    </ngt-primitive>
+  `,
+  imports: [NgtArgs],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class NgtsText extends NgtRxStore implements OnInit, OnDestroy {
+  static [NgtRendererFlags.COMPOUND] = true;
+
+  @Input() ref = injectNgtRef<Text>();
+
+  @Input() set text(text: string) {
+    this.set({ text });
+  }
+
+  @Input() set characters(characters: string) {
+    this.set({ characters });
+  }
+
+  @Input() set font(font: string) {
+    this.set({ font });
+  }
+
+  @Input() set anchorX(anchorX: number | 'left' | 'center' | 'right') {
+    this.set({ anchorX });
+  }
+
+  @Input() set anchorY(
+    anchorY: number | 'top' | 'top-baseline' | 'middle' | 'bottom-baseline' | 'bottom'
+  ) {
+    this.set({ anchorY });
+  }
+
+  @Output() sync = new EventEmitter<Text>();
+
+  override initialize(): void {
+    super.initialize();
+    this.set({ anchorX: 'center', anchorY: 'middle', text: '' });
+  }
+
+  readonly #store = injectNgtStore();
+  readonly #actions = inject(RxActionFactory<{ preloadFont: void }>).create();
+  readonly #text = new Text();
+
+  ngOnInit(): void {
+    if (!this.ref.nativeElement) this.ref.nativeElement = this.#text;
+    this.#preloadFont();
+    this.#sync();
+  }
+
+  override ngOnDestroy(): void {
+    this.#text.dispose();
+    super.ngOnDestroy();
+  }
+
+  #preloadFont() {
+    this.hold(this.#actions.preloadFont$, () => {
+      const { font, characters } = this.get();
+      if (font && characters) {
+        preloadFont({ font, characters });
+      }
+    });
+    this.#actions.preloadFont();
+  }
+
+  #sync() {
+    this.hold(this.select(), () => {
+      const invalidate = this.#store.get('invalidate');
+      this.#text.sync(() => {
+        invalidate();
+        if (this.sync.observed) this.sync.next(this.#text);
+      });
+    });
+  }
+}
