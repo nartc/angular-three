5b46ba583a0411634100fbaf8827eae32a228a68
Chau Tran <nartc7789@gmail.com>
2023-01-06 13:21:19 -0600

    new portal api for view cube

--- packages/sandbox/src/app/view-cube/scene.component.ts
+++ packages/sandbox/src/app/view-cube/scene.component.ts
@@ -8,23 +8,22 @@ import {
   NgtPortalContent,
   NgtPush,
   NgtRef,
+  NgtRenderState,
   NgtRepeat,
   NgtRxStore,
   NgtVector3,
-  prepare,
 } from '@angular-three/core';
+import { NgtsOrthographicCamera } from '@angular-three/soba/cameras';
 import { NgtsOrbitControls } from '@angular-three/soba/controls';
-import { injectNgtCameraRaycast } from '@angular-three/soba/misc';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
-import { RxActionFactory } from '@rx-angular/state/actions';
-import * as THREE from 'three';
+import { NgtsEnvironment } from '@angular-three/soba/staging';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {
   AmbientLight,
   BoxGeometry,
   Matrix4,
   Mesh,
   MeshLambertMaterial,
-  MeshNormalMaterial,
+  MeshStandardMaterial,
   PointLight,
   TorusGeometry,
 } from 'three';
@@ -36,21 +35,24 @@ extend({
   AmbientLight,
   PointLight,
   TorusGeometry,
-  MeshNormalMaterial,
+  MeshStandardMaterial,
 });
 
 @Component({
   selector: 'view-cube',
   standalone: true,
   template: `
-    <ngt-portal [container]="virtualSceneRef">
+    <ngt-portal (beforeRender)="onBeforeRender($event.root)">
       <ng-template ngtPortalContent>
+        <ngts-orthographic-camera
+          [makeDefault]="true"
+          [position]="[0, 0, 100]"
+        ></ngts-orthographic-camera>
         <ngt-mesh
           *ref="meshRef"
-          [raycast]="cameraRaycast"
           [position]="position$ | ngtPush : [0, 0, 0]"
           (pointerout)="hovered = -1"
-          (pointermove)="onPointerMove($any($event).faceIndex)"
+          (pointermove)="hovered = Math.floor(($any($event).faceIndex || 0) / 2)"
         >
           <ng-container *ngFor="let i; repeat: 6">
             <ngt-mesh-lambert-material
@@ -61,64 +63,41 @@ extend({
           </ng-container>
           <ngt-box-geometry *args="[60, 60, 60]"></ngt-box-geometry>
         </ngt-mesh>
-        <ngt-ambient-light intensity="0.5"></ngt-ambient-light>
-        <ngt-point-light [position]="10" intensity="0.5"></ngt-point-light>
+        <ngt-ambient-light intensity="1"></ngt-ambient-light>
+        <ngt-point-light [position]="[200, 200, 100]" intensity="0.5"></ngt-point-light>
       </ng-template>
     </ngt-portal>
   `,
-  imports: [NgtPortal, NgtPortalContent, NgtPush, NgtArgs, NgtRepeat, NgtRef, NgtAttachArray],
-  providers: [RxActionFactory],
+  imports: [
+    NgtPortal,
+    NgtPortalContent,
+    NgtPush,
+    NgtArgs,
+    NgtRepeat,
+    NgtRef,
+    NgtAttachArray,
+    NgtsOrthographicCamera,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ViewCube extends NgtRxStore implements OnInit {
+export class ViewCube extends NgtRxStore {
   readonly #store = injectNgtStore();
-  readonly #actions$ = inject(RxActionFactory<{ setBeforeRender: void }>).create();
+  readonly #matrix = new Matrix4();
 
-  readonly events = this.#store.get('events');
-
+  readonly Math = Math;
   readonly position$ = this.#store.select(
     'size',
     (size) => [size.width / 2 - 80, size.height / 2 - 80, 0] as NgtVector3
   );
-
   readonly meshRef = injectNgtRef<Mesh>();
-  readonly virtualCam = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000);
-  readonly cameraRaycast = injectNgtCameraRaycast(this.virtualCam);
-  readonly virtualSceneRef = injectNgtRef<THREE.Scene>(prepare(new THREE.Scene()));
 
   hovered = -1;
 
-  ngOnInit() {
-    this.hold(this.#store.select('size'), () => {
-      const size = this.#store.get('size');
-      this.virtualCam.left = -size.width / 2;
-      this.virtualCam.right = size.width / 2;
-      this.virtualCam.top = size.height / 2;
-      this.virtualCam.bottom = -size.height / 2;
-      this.virtualCam.position.set(0, 0, 100);
-      this.virtualCam.updateProjectionMatrix();
-    });
-
-    this.effect(this.#actions$.setBeforeRender$, () => {
-      const matrix = new Matrix4();
-      return this.#store.get('internal').subscribe(({ scene, camera, gl }) => {
-        if (this.meshRef.nativeElement) {
-          matrix.copy(camera.matrix).invert();
-          this.meshRef.nativeElement.quaternion.setFromRotationMatrix(matrix);
-          gl.autoClear = true;
-          gl.render(scene, camera);
-          gl.autoClear = false;
-          gl.clearDepth();
-          gl.render(this.virtualSceneRef.nativeElement, this.virtualCam);
-        }
-      }, 1);
-    });
-
-    this.#actions$.setBeforeRender();
-  }
-
-  onPointerMove(faceIndex?: number) {
-    this.hovered = Math.floor((faceIndex || 0) / 2);
+  onBeforeRender({ camera }: NgtRenderState) {
+    if (this.meshRef.nativeElement) {
+      this.#matrix.copy(camera.matrix).invert();
+      this.meshRef.nativeElement.quaternion.setFromRotationMatrix(this.#matrix);
+    }
   }
 }
 
@@ -126,14 +105,16 @@ export class ViewCube extends NgtRxStore implements OnInit {
   selector: 'view-cube-scene',
   standalone: true,
   template: `
-    <ngt-mesh>
-      <ngt-torus-geometry *args="[1, 0.5, 32, 100]"></ngt-torus-geometry>
-      <ngt-mesh-normal-material></ngt-mesh-normal-material>
+    <ngt-ambient-light intensity="0.5"></ngt-ambient-light>
+    <ngt-mesh scale="2">
+      <ngt-torus-geometry *args="[1, 0.25, 32, 100]"></ngt-torus-geometry>
+      <ngt-mesh-standard-material></ngt-mesh-standard-material>
     </ngt-mesh>
     <ngts-orbit-controls></ngts-orbit-controls>
+    <ngts-environment preset="city"></ngts-environment>
     <view-cube></view-cube>
   `,
-  imports: [ViewCube, NgtArgs, NgtsOrbitControls],
+  imports: [ViewCube, NgtArgs, NgtsOrbitControls, NgtsEnvironment],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Scene {}
