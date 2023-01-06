4d99bfc39fbda7529606530e06ca8fd906638193
Chau Tran <nartc7789@gmail.com>
2023-01-05 20:46:35 -0600

    update keen with some more effects

--- packages/sandbox/src/app/keen-bloom/scene.component.ts
+++ packages/sandbox/src/app/keen-bloom/scene.component.ts
@@ -1,6 +1,6 @@
 import { extend, NgtArgs, NgtPush } from '@angular-three/core';
 import { NgtpEffectComposer } from '@angular-three/postprocessing';
-import { NgtpBloom } from '@angular-three/postprocessing/effects';
+import { NgtpDepth, NgtpDotScreen } from '@angular-three/postprocessing/effects';
 import { NgtsOrbitControls } from '@angular-three/soba/controls';
 import { injectNgtsGLTFLoader } from '@angular-three/soba/loaders';
 import { NgIf } from '@angular/common';
@@ -54,10 +54,11 @@ export class Keen {
     <ngt-directional-light [position]="[0, 1, 2]" color="white"></ngt-directional-light>
 
     <ngtp-effect-composer>
-      <ngtp-bloom></ngtp-bloom>
+      <ngtp-depth></ngtp-depth>
+      <ngtp-dot-screen></ngtp-dot-screen>
     </ngtp-effect-composer>
   `,
-  imports: [Keen, NgtsOrbitControls, NgtpEffectComposer, NgtpBloom, NgtArgs],
+  imports: [Keen, NgtsOrbitControls, NgtpEffectComposer, NgtpDepth, NgtpDotScreen, NgtArgs],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Scene {}
